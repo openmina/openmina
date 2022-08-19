a21b98f2062d9f1c82ce16f34d83654fbfb5d5df
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2022-08-19 12:25:17 +0300

    Implement fuzzcheck targets for gossip messages
    Fixes: #6

---
+++ mina-p2p-messages/tests/read-fuzz.rs
@@ -0,0 +1,43 @@
+#![cfg(all(test, fuzzing))]
+
+fn try_decode<T>(mut buf: &[u8]) -> bool
+where
+    T: binprot::BinProtRead,
+{
+    match T::binprot_read(&mut buf) {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+    true
+}
+
+fn fuzz<F>(f: F)
+where
+    F: Fn(&[u8]) -> bool + 'static,
+{
+    let result = fuzzcheck::fuzz_test(f)
+        .default_options()
+        .stop_after_first_test_failure(true)
+        .launch();
+    assert!(!result.found_test_failure);
+}
+
+#[test]
+fn gossip_message() {
+    fuzz(try_decode::<mina_p2p_messages::GossipNetMessage>);
+}
+
+#[test]
+fn external_transition() {
+    fuzz(try_decode::<mina_p2p_messages::p2p::MinaBlockExternalTransitionRawVersionedStable>);
+}
+
+#[test]
+fn snark_pool_diff() {
+    fuzz(try_decode::<mina_p2p_messages::p2p::NetworkPoolSnarkPoolDiffVersionedStable>);
+}
+
+#[test]
+fn tx_pool_diff() {
+    fuzz(try_decode::<mina_p2p_messages::p2p::NetworkPoolTransactionPoolDiffVersionedStable>);
+}
