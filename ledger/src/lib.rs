0f7436fe0f499ed0c795290b89bf596045e27bf1
Sebastien Chapuis <sebastien@chapu.is>
2022-09-20 11:39:34 +0200

    Run tests in all drivers (chrome, firefox, node)

--- ledger/src/lib.rs
+++ ledger/src/lib.rs
@@ -1,5 +1,14 @@
 #![allow(dead_code)]
 
+// We need a feature to tests both nodejs and browser
+// https://github.com/rustwasm/wasm-bindgen/issues/2571
+#[cfg(not(feature = "in_nodejs"))]
+#[cfg(target_family = "wasm")]
+mod wasm {
+    use wasm_bindgen_test::*;
+    wasm_bindgen_test_configure!(run_in_browser);
+}
+
 #[cfg(not(target_family = "wasm"))]
 mod ffi;
 
