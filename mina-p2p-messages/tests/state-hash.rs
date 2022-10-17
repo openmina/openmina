34c0af30055e866c5258b1b51a72e4403a80a7f9
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2022-10-17 18:29:07 +0300

    Refactor state-hash test

--- mina-p2p-messages/tests/state-hash.rs
+++ mina-p2p-messages/tests/state-hash.rs
@@ -9,8 +9,17 @@ mod tests {
 
     #[test]
     fn state_hash() {
-        utils::for_all("v1/gossip/external-transition", |file_path, mut encoded| {
-            let file_path = file_path.to_string_lossy();
+        let data: [(&[u8], &str); 2] = [
+            (
+                include_bytes!("files/v1/gossip/external-transition/1.bin"),
+                "3NLUiFWnakJxjUhFoNw1PPSRuRgJbhmfHqSLweEWrxcPpo8ikVTb",
+            ),
+            (
+                include_bytes!("files/v1/gossip/external-transition/2.bin"),
+                "3NL7hhVbBfxhWfyUscv6LrqfvxgHGfQVPMi8ynDMZyyDN2GhqDbc",
+            ),
+        ];
+        for (mut encoded, expected_hash) in data {
             let external_transition =
                 MinaBlockExternalTransitionRawVersionedStableV1Versioned::binprot_read(
                     &mut encoded,
@@ -22,20 +31,7 @@ mod tests {
             let mut hasher = mina_hasher::create_legacy(());
             let hash = external_transition.inner().protocol_state.hash(&mut hasher);
 
-            if file_path.ends_with("v1/gossip/external-transition/1.bin") {
-                assert_eq!(
-                    hash.to_string(),
-                    "3NLUiFWnakJxjUhFoNw1PPSRuRgJbhmfHqSLweEWrxcPpo8ikVTb".to_owned()
-                );
-            } else if file_path.ends_with("v1/gossip/external-transition/2.bin") {
-                assert_eq!(
-                    hash.to_string(),
-                    "3NL7hhVbBfxhWfyUscv6LrqfvxgHGfQVPMi8ynDMZyyDN2GhqDbc".to_owned()
-                );
-            } else {
-                panic!("Unknown test file. Is it new? Add it to `tests/state-hash.rs`");
-            }
-        })
-        .unwrap();
+            assert_eq!(&hash.to_string(), expected_hash);
+        }
     }
 }
