208fcad032e439fdd43a4f870063e0177d6c1c5b
Sebastien Chapuis <sebastien@chapu.is>
2022-11-15 10:33:46 +0100

    Start implementation of `derive_plonk`

--- ledger/src/blocks/mod.rs
+++ ledger/src/blocks/mod.rs
@@ -1,10 +1,12 @@
 mod conv;
+mod plonk_checks;
 mod prepared_statement;
 mod protocol_state;
 mod scalar_challenge;
 mod verify;
 
 pub use conv::*;
+pub use plonk_checks::*;
 pub use prepared_statement::*;
 pub use protocol_state::*;
 pub use scalar_challenge::*;
