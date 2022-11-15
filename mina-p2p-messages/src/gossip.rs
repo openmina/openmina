ff1464d893e33b4d50ee01a863e21c9a48585d08
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2022-11-15 17:34:57 +0300

    Use correct names for gossip messages

--- mina-p2p-messages/src/gossip.rs
+++ mina-p2p-messages/src/gossip.rs
@@ -4,23 +4,17 @@ use serde::{Deserialize, Serialize};
 use crate::{v1, v2};
 
 #[derive(Clone, Debug, Serialize, Deserialize, BinProtRead, BinProtWrite, PartialEq)]
-#[serde(tag = "type", content = "message")]
+#[serde(tag = "type", content = "message", rename_all = "snake_case")]
 pub enum GossipNetMessageV1 {
-    #[serde(rename = "external_transition")]
     NewState(v1::MinaBlockExternalTransitionRawVersionedStableV1Versioned),
-    #[serde(rename = "snark_pool_diff")]
     SnarkPoolDiff(v1::NetworkPoolSnarkPoolDiffVersionedStableV1Versioned),
-    #[serde(rename = "transaction_pool_diff")]
     TransactionPoolDiff(v1::NetworkPoolTransactionPoolDiffVersionedStableV1Versioned),
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, BinProtRead, BinProtWrite, PartialEq)]
-#[serde(tag = "type", content = "message")]
+#[serde(tag = "type", content = "message", rename_all = "snake_case")]
 pub enum GossipNetMessageV2 {
-    #[serde(rename = "external_transition")]
     NewState(v2::MinaBlockBlockStableV2),
-    #[serde(rename = "snark_pool_diff")]
     SnarkPoolDiff(v2::NetworkPoolSnarkPoolDiffVersionedStableV2),
-    #[serde(rename = "transaction_pool_diff")]
     TransactionPoolDiff(v2::NetworkPoolTransactionPoolDiffVersionedStableV2),
 }
