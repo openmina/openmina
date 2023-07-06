acf41d142e653be056a50ed27752623042c3e760
Zura Benashvili <zura.benashvili@gmail.com>
2023-07-06 09:40:28 +0400

    feat(staged_ledger): make copy function public

--- ledger/src/staged_ledger/staged_ledger.rs
+++ ledger/src/staged_ledger/staged_ledger.rs
@@ -417,7 +417,7 @@ impl StagedLedger {
     }
 
     /// https://github.com/MinaProtocol/mina/blob/05c2f73d0f6e4f1341286843814ce02dcb3919e0/src/lib/staged_ledger/staged_ledger.ml#L393
-    fn copy(&self) -> Self {
+    pub fn copy(&self) -> Self {
         let new_mask = self.ledger.make_child();
 
         Self {
