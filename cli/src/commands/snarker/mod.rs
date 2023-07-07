bf56c1360081ad33b2d38d1b445f391d2ca583ee
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2023-07-07 16:22:18 +0300

    Add naive monotonic time for tracing

--- cli/src/commands/snarker/mod.rs
+++ cli/src/commands/snarker/mod.rs
@@ -3,8 +3,9 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use rand::prelude::*;
-use serde::Serialize;
 use snarker::p2p::service_impl::TaskSpawner;
+use shared::info;
+use shared::log::inner::Level;
 use tokio::select;
 use tokio::sync::{mpsc, oneshot};
 
@@ -34,6 +35,7 @@ mod http_server;
 
 mod rpc;
 use rpc::RpcP2pConnectionOutgoingResponse;
+mod tracing;
 
 /// Openmina snarker
 #[derive(Debug, clap::Args)]
@@ -50,15 +52,16 @@ pub struct Snarker {
     /// Port to listen to
     #[arg(long, short, default_value = "3000")]
     pub port: u16,
+
+    /// Verbosity level
+    #[arg(long, short, default_value = "info")]
+    pub verbosity: Level,
 }
 
 impl Snarker {
     pub fn run(self) -> Result<(), crate::CommandError> {
-        // stderrlog::new()
-        //     .verbosity(5)
-        //     // .module(module_path!()) // remove this line to see libp2p logs.
-        //     .init()
-        //     .unwrap();
+        tracing::initialize(self.verbosity);
+
         if let Err(ref e) = rayon::ThreadPoolBuilder::new()
             .num_threads(num_cpus::get().max(2) - 1)
             .build_global()
@@ -80,7 +83,8 @@ impl Snarker {
         let secret_key = SecretKey::from_bytes(bytes);
         let pub_key = secret_key.public_key();
         let peer_id = PeerId::from_public_key(pub_key.clone());
-        eprintln!("peer_id: {peer_id}");
+
+        info!(shared::log::system_time(); peer_id = format!("{peer_id}"));
 
         let config = Config {
             ledger: LedgerConfig {},
