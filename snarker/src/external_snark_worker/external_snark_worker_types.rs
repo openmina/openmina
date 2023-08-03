7898a9f43255effdb8eb936b2652a4c6606975bb
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2023-08-03 14:45:42 +0300

    feat(snarker/ext): fixes and improvements
    - fix missing await in reading result from snark worker
    - using just proof as a result in action
    - added external snarker test

--- snarker/src/external_snark_worker/external_snark_worker_types.rs
+++ snarker/src/external_snark_worker/external_snark_worker_types.rs
@@ -1,14 +1,13 @@
+use std::sync::Arc;
+
 use mina_p2p_messages::v2::{
-    SnarkWorkerWorkerRpcsVersionedGetWorkV2TResponse,
-    SnarkWorkerWorkerRpcsVersionedSubmitWorkV2TQuery,
+    SnarkWorkerWorkerRpcsVersionedGetWorkV2TResponse, TransactionSnarkWorkTStableV2Proofs,
 };
 use shared::snark_job_id::SnarkJobId;
 
-/// TODO
 pub type SnarkWorkId = SnarkJobId;
 
-/// TODO
+/// TODO use more slim type `OneOrTwo`...
 pub type SnarkWorkSpec = SnarkWorkerWorkerRpcsVersionedGetWorkV2TResponse;
 
-/// TODO
-pub type SnarkWorkResult = SnarkWorkerWorkerRpcsVersionedSubmitWorkV2TQuery;
+pub type SnarkWorkResult = Arc<TransactionSnarkWorkTStableV2Proofs>;
