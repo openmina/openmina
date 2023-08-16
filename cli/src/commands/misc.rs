ac1b914d005bc47166997e779e305c90ed35bfc1
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2023-08-16 17:30:45 +0300

    feat(snarker): add misc mina-key-pair command

--- cli/src/commands/misc.rs
+++ cli/src/commands/misc.rs
@@ -1,5 +1,4 @@
-use rand::{rngs::ThreadRng, Rng};
-use snarker::p2p::identity::SecretKey;
+use snarker::{account::AccountSecretKey, p2p::identity::SecretKey};
 
 use crate::CommandError;
 
@@ -12,7 +11,8 @@ pub struct Misc {
 impl Misc {
     pub fn run(self) -> Result<(), CommandError> {
         match self.command {
-            MiscCommand::P2PKeyPair(command) => command.run()
+            MiscCommand::P2PKeyPair(command) => command.run(),
+            MiscCommand::MinaKeyPair(command) => command.run(),
         }
     }
 }
@@ -20,23 +20,18 @@ impl Misc {
 #[derive(Clone, Debug, clap::Subcommand)]
 pub enum MiscCommand {
     P2PKeyPair(P2PKeyPair),
-}
-
-fn random_sk() -> SecretKey {
-    let mut rng = ThreadRng::default();
-    let bytes = rng.gen();
-    SecretKey::from_bytes(bytes)
+    MinaKeyPair(MinaKeyPair),
 }
 
 #[derive(Debug, Clone, clap::Args)]
 pub struct P2PKeyPair {
-    #[arg(long, short = 's', env="OPENMINA_P2P_SEC_KEY")]
+    #[arg(long, short = 's', env = "OPENMINA_P2P_SEC_KEY")]
     p2p_secret_key: Option<SecretKey>,
 }
 
 impl P2PKeyPair {
     pub fn run(self) -> Result<(), CommandError> {
-        let secret_key = self.p2p_secret_key.unwrap_or_else(random_sk);
+        let secret_key = self.p2p_secret_key.unwrap_or_else(SecretKey::rand);
         let public_key = secret_key.public_key();
         let peer_id = public_key.peer_id();
         let libp2p_peer_id = libp2p::PeerId::from(peer_id);
@@ -44,6 +39,23 @@ impl P2PKeyPair {
         println!("public key: {public_key}");
         println!("peer_id:    {peer_id}");
         println!("libp2p_id:  {libp2p_peer_id}");
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, clap::Args)]
+pub struct MinaKeyPair {
+    #[arg(long, short = 's', env = "OPENMINA_SEC_KEY")]
+    secret_key: Option<AccountSecretKey>,
+}
+
+impl MinaKeyPair {
+    pub fn run(self) -> Result<(), CommandError> {
+        let secret_key = self.secret_key.unwrap_or_else(AccountSecretKey::rand);
+        let public_key = secret_key.public_key();
+        println!("secret key: {secret_key}");
+        println!("public key: {public_key}");
 
         Ok(())
     }
