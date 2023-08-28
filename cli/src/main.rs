fc55b7b193a07378cec483b1e3280d7859b9c3d4
Zura Benashvili <zura.benashvili@gmail.com>
2023-08-28 18:45:14 +0400

    feat(cli): optional SIGSEGV handler so that we can make sure recorder's data is flushed to disk

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,7 +5,44 @@ pub use commands::CommandError;
 mod exit_with_error;
 pub use exit_with_error::exit_with_error;
 
+#[cfg(feature = "unsafe-signal-handlers")]
+mod unsafe_signal_handlers {
+    use nix::libc;
+
+    extern "C" fn handle_sigsegv(_signal: libc::c_int) {
+        eprintln!("########### SIGSEGV #############");
+        snarker::recorder::Recorder::graceful_shutdown();
+        std::process::exit(1);
+    }
+
+    pub fn setup() {
+        let stack_t = libc::stack_t {
+            ss_sp: {
+                let stack = Box::<[u8; libc::SIGSTKSZ]>::new([0; libc::SIGSTKSZ]);
+                Box::into_raw(stack) as *mut _
+            },
+            ss_flags: 0,
+            ss_size: libc::SIGSTKSZ,
+        };
+
+        let res = unsafe { libc::sigaltstack(&stack_t as *const _, std::ptr::null_mut()) };
+        assert_eq!(res, 0);
+
+        let action = libc::sigaction {
+            sa_sigaction: handle_sigsegv as _,
+            sa_mask: unsafe { std::mem::zeroed() },
+            sa_flags: libc::SA_ONSTACK,
+            sa_restorer: None,
+        };
+        let res = unsafe { libc::sigaction(libc::SIGSEGV, &action as _, std::ptr::null_mut()) };
+        assert_eq!(res, 0);
+    }
+}
+
 fn main() {
+    #[cfg(feature = "unsafe-signal-handlers")]
+    unsafe_signal_handlers::setup();
+
     match commands::OpenminaCli::parse().command.run() {
         Ok(_) => {}
         Err(err) => exit_with_error(err),
