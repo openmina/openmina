817bb52a92594d067825e168fce0149b47ce7196
Zura Benashvili <zura.benashvili@gmail.com>
2023-08-28 18:45:14 +0400

    feat(cli): optional SIGSEGV handler so that we can make sure recorder's data is flushed to disk

--- snarker/src/recorder/recorder.rs
+++ snarker/src/recorder/recorder.rs
@@ -2,16 +2,19 @@ use std::borrow::Cow;
 use std::fs;
 use std::io::{BufWriter, Write};
 use std::path::{Path, PathBuf};
+use std::sync::{Mutex, TryLockError};
 
 use crate::{Action, ActionWithMeta, EventSourceAction, State};
 
 use super::{RecordedActionWithMeta, RecordedInitialState};
 
+static ACTIONS_F: Mutex<Option<fs::File>> = Mutex::new(None);
+
+/// There must only be 1 `Recorder` instance per process!
 pub enum Recorder {
     None,
     OnlyInputActions {
         recorder_path: PathBuf,
-        actions_f: fs::File,
         actions_f_bytes_written: u64,
         actions_f_index: usize,
     },
@@ -27,10 +30,12 @@ impl Recorder {
         let actions_f_index = 1;
         let actions_path = super::actions_path(&path, actions_f_index);
 
+        let file = fs::File::create(actions_path)
+            .expect("creating file for openmina recorder initial state failed!");
+        let _ = ACTIONS_F.try_lock().unwrap().insert(file);
+
         Self::OnlyInputActions {
             recorder_path: path,
-            actions_f: fs::File::create(actions_path)
-                .expect("creating file for openmina recorder initial state failed!"),
             actions_f_bytes_written: 0,
             actions_f_index,
         }
@@ -58,7 +63,6 @@ impl Recorder {
             Self::None => {}
             Self::OnlyInputActions {
                 recorder_path,
-                actions_f,
                 actions_f_bytes_written,
                 actions_f_index,
                 ..
@@ -79,16 +83,18 @@ impl Recorder {
                     RecordedActionWithMeta::from(action)
                 };
 
+                let mut cur_f = ACTIONS_F.try_lock().unwrap();
+
                 let file = if *actions_f_bytes_written > 64 * 1024 * 1024 {
-                    actions_f.sync_all().unwrap();
+                    cur_f.take().unwrap().sync_all().unwrap();
                     *actions_f_bytes_written = 0;
                     *actions_f_index += 1;
-                    *actions_f =
+                    cur_f.insert(
                         fs::File::create(super::actions_path(recorder_path, *actions_f_index))
-                            .unwrap();
-                    actions_f
+                            .unwrap(),
+                    )
                 } else {
-                    actions_f
+                    cur_f.as_mut().unwrap()
                 };
 
                 let mut writer = BufWriter::new(file);
@@ -104,16 +110,35 @@ impl Recorder {
             }
         }
     }
+
+    pub fn graceful_shutdown() {
+        graceful_shutdown()
+    }
 }
 
 impl Drop for Recorder {
     fn drop(&mut self) {
         match self {
             Self::None => {}
-            Self::OnlyInputActions { actions_f, .. } => {
-                eprintln!("Flushing recorded actions to disk before shutdown");
-                let _ = actions_f.sync_all();
+            Self::OnlyInputActions { .. } => {
+                graceful_shutdown();
             }
         }
     }
+}
+
+pub fn graceful_shutdown() {
+    let Some(f) = ACTIONS_F
+        .try_lock()
+        .map(|mut v| v.take())
+        .unwrap_or_else(|err| match err {
+            TryLockError::WouldBlock => None,
+            TryLockError::Poisoned(v) => v.into_inner().take(),
+        })
+    else {
+        return;
+    };
+
+    eprintln!("Flushing recorded actions to disk before shutdown");
+    let _ = f.sync_all();
 }
