openmina/mina-p2p-messages-rs

Implement `Deref` for tuples having a single data

src/b58.rs
@@ -98,6 +98,14 @@ impl<T, U, const V: u8> From<T> for AsBase58Check<T, U, V> {
     }
 }
 
+impl<T, U, const V: u8> std::ops::Deref for AsBase58Check<T, U, V> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 impl<T, U, const V: u8> AsBase58Check<T, U, V> {
     pub fn into_inner(self) -> T {
         self.0

openmina/openmina

mina-p2p-messages/src/b58.rs
@@ -100,6 +100,14 @@ impl<T, U, const V: u8> From<T> for Base58CheckOfBinProt<T, U, V> {
     }
 }
 
+impl<T, U, const V: u8> std::ops::Deref for Base58CheckOfBinProt<T, U, V> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 impl<T, U, const V: u8> Base58CheckOfBinProt<T, U, V> {
     pub fn into_inner(self) -> T {
         self.0