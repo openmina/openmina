openmina/openmina

commit 09a9b7612e9e87fef2a16bba0b0415e104a94020

feat(health): implement healthz RPC
Re: #94

node/native/src/rpc.rs
@@ -1,3 +1,4 @@
+use node::rpc::RpcHealthCheckResponse;
 use serde::{Deserialize, Serialize};
 
 use node::core::channels::{mpsc, oneshot};
@@ -64,6 +65,21 @@ impl NodeService {
     }
 }
 
+macro_rules! rpc_service_impl {
+    ($name:ident, $ty:ty) => {
+        fn $name(&mut self, rpc_id: RpcId, response: $ty) -> Result<(), RespondError> {
+            let entry = self.rpc.pending.remove(rpc_id);
+            let chan = entry.ok_or(RespondError::UnknownRpcId)?;
+            let chan = chan
+                .downcast::<oneshot::Sender<$ty>>()
+                .or(Err(RespondError::UnexpectedResponseType))?;
+            chan.send(response)
+                .or(Err(RespondError::RespondingFailed))?;
+            Ok(())
+        }
+    };
+}
+
 impl node::rpc::RpcService for NodeService {
     fn respond_state_get(&mut self, rpc_id: RpcId, response: &State) -> Result<(), RespondError> {
         let entry = self.rpc.pending.remove(rpc_id);
@@ -76,66 +92,16 @@ impl node::rpc::RpcService for NodeService {
         Ok(())
     }
 
-    fn respond_sync_stats_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcSyncStatsGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcSyncStatsGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_action_stats_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcActionStatsGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcActionStatsGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_p2p_connection_outgoing(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcP2pConnectionOutgoingResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcP2pConnectionOutgoingResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_p2p_connection_incoming_answer(
-        &mut self,
-        rpc_id: RpcId,
-        response: P2pConnectionResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.get(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast_ref::<mpsc::Sender<RpcP2pConnectionIncomingResponse>>()
-            .ok_or(RespondError::UnexpectedResponseType)?
-            .clone();
-        chan.try_send(RpcP2pConnectionIncomingResponse::Answer(response))
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
+    rpc_service_impl!(respond_sync_stats_get, RpcSyncStatsGetResponse);
+    rpc_service_impl!(respond_action_stats_get, RpcActionStatsGetResponse);
+    rpc_service_impl!(
+        respond_p2p_connection_outgoing,
+        RpcP2pConnectionOutgoingResponse
+    );
+    rpc_service_impl!(
+        respond_p2p_connection_incoming_answer,
+        P2pConnectionResponse
+    );
 
     fn respond_p2p_connection_incoming(
         &mut self,
@@ -152,108 +118,24 @@ impl node::rpc::RpcService for NodeService {
         Ok(())
     }
 
-    fn respond_scan_state_summary_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcScanStateSummaryGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcScanStateSummaryGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snark_pool_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcSnarkPoolGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcSnarkPoolGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snark_pool_job_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcSnarkPoolJobGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcSnarkPoolJobGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snarker_job_commit(
-        &mut self,
-        rpc_id: RpcId,
-        response: RpcSnarkerJobCommitResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcSnarkerJobCommitResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response)
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snarker_job_spec(
-        &mut self,
-        rpc_id: RpcId,
-        response: node::rpc::RpcSnarkerJobSpecResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<RpcSnarkerJobSpecResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response.clone())
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snarker_workers(
-        &mut self,
-        rpc_id: RpcId,
-        response: node::rpc::RpcSnarkerWorkersResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<node::rpc::RpcSnarkerWorkersResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response.clone())
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
-
-    fn respond_snarker_config_get(
-        &mut self,
-        rpc_id: RpcId,
-        response: node::rpc::RpcSnarkerConfigGetResponse,
-    ) -> Result<(), RespondError> {
-        let entry = self.rpc.pending.remove(rpc_id);
-        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
-        let chan = chan
-            .downcast::<oneshot::Sender<node::rpc::RpcSnarkerConfigGetResponse>>()
-            .or(Err(RespondError::UnexpectedResponseType))?;
-        chan.send(response.clone())
-            .or(Err(RespondError::RespondingFailed))?;
-        Ok(())
-    }
+    rpc_service_impl!(
+        respond_scan_state_summary_get,
+        RpcScanStateSummaryGetResponse
+    );
+    rpc_service_impl!(respond_snark_pool_get, RpcSnarkPoolGetResponse);
+    rpc_service_impl!(respond_snark_pool_job_get, RpcSnarkPoolJobGetResponse);
+    rpc_service_impl!(respond_snarker_job_commit, RpcSnarkerJobCommitResponse);
+    rpc_service_impl!(
+        respond_snarker_job_spec,
+        node::rpc::RpcSnarkerJobSpecResponse
+    );
+    rpc_service_impl!(
+        respond_snarker_workers,
+        node::rpc::RpcSnarkerWorkersResponse
+    );
+    rpc_service_impl!(
+        respond_snarker_config_get,
+        node::rpc::RpcSnarkerConfigGetResponse
+    );
+    rpc_service_impl!(respond_health_check, RpcHealthCheckResponse);
 }

node/src/rpc/rpc_actions.rs
@@ -41,6 +41,8 @@ pub enum RpcAction {
 
     SnarkerWorkersGet(RpcSnarkersWorkersGetAction),
 
+    HealthCheck(RpcHealthCheckAction),
+
     Finish(RpcFinishAction),
 }
 
@@ -252,6 +254,14 @@ pub struct RpcSnarkersWorkersGetAction {
 
 impl redux::EnablingCondition<crate::State> for RpcSnarkersWorkersGetAction {}
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RpcHealthCheckAction {
+    pub rpc_id: RpcId,
+}
+
+impl redux::EnablingCondition<crate::State> for RpcHealthCheckAction {}
+
+
 /// Finish/Cleanup rpc request.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RpcFinishAction {
@@ -295,6 +305,8 @@ impl_into_global_action!(
     RpcSnarkerJobSpecAction,
 
     RpcSnarkersWorkersGetAction,
+
+    RpcHealthCheckAction,
 
     RpcFinishAction,
 );

node/native/src/http_server.rs
@@ -6,7 +6,7 @@ use warp::{
     http::HeaderValue,
     hyper::{header::CONTENT_TYPE, Response, StatusCode},
     reply::with_status,
-    Filter, Reply,
+    Filter, Reply, Rejection,
 };
 
 use node::{
@@ -354,9 +354,35 @@ pub async fn run(port: u16, rpc_sender: super::RpcSender) {
         .or(snarker_job_commit)
         .or(snarker_job_spec)
         .or(snark_workers)
+        .or(healthcheck(rpc_sender.clone()))
         .or(super::graphql::routes(rpc_sender))
         .with(cors);
     warp::serve(routes).run(([0, 0, 0, 0], port)).await;
+}
+
+const DROPPED_CHANNEL: &str = "response channel dropped";
+
+fn healthcheck(rpc_sender: super::RpcSender) -> impl Filter<Error = Rejection, Extract = impl Reply> + Clone {
+    warp::path!("healthz").and(warp::get()).then(move || {
+        let rpc_sender = rpc_sender.clone();
+        async move {
+            rpc_sender
+                .oneshot_request(RpcRequest::HealthCheck)
+                .await
+                .map_or_else(
+                    || {
+                        with_status(
+                            String::from(DROPPED_CHANNEL),
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                        )
+                    },
+                    |reply: node::rpc::RpcHealthCheckResponse| match reply {
+                        Ok(()) => with_status(String::new(), StatusCode::OK),
+                        Err(err) => with_status(err, StatusCode::SERVICE_UNAVAILABLE),
+                    },
+                )
+        }
+    })
 }
 
 use warp::filters::BoxedFilter;

node/src/action_kind.rs
@@ -95,7 +95,7 @@ use crate::p2p::peer::{P2pPeerAction, P2pPeerBestTipUpdateAction, P2pPeerReadyAc
 use crate::p2p::P2pAction;
 use crate::rpc::{
     RpcAction, RpcActionStatsGetAction, RpcFinishAction, RpcGlobalStateGetAction,
-    RpcP2pConnectionIncomingErrorAction, RpcP2pConnectionIncomingInitAction,
+    RpcHealthCheckAction, RpcP2pConnectionIncomingErrorAction, RpcP2pConnectionIncomingInitAction,
     RpcP2pConnectionIncomingPendingAction, RpcP2pConnectionIncomingRespondAction,
     RpcP2pConnectionIncomingSuccessAction, RpcP2pConnectionOutgoingErrorAction,
     RpcP2pConnectionOutgoingInitAction, RpcP2pConnectionOutgoingPendingAction,
@@ -295,6 +295,7 @@ pub enum ActionKind {
     RpcActionStatsGet,
     RpcFinish,
     RpcGlobalStateGet,
+    RpcHealthCheck,
     RpcP2pConnectionIncomingError,
     RpcP2pConnectionIncomingInit,
     RpcP2pConnectionIncomingPending,
@@ -397,7 +398,7 @@ pub enum ActionKind {
 }
 
 impl ActionKind {
-    pub const COUNT: u16 = 199;
+    pub const COUNT: u16 = 200;
 }
 
 impl std::fmt::Display for ActionKind {
@@ -524,6 +525,7 @@ impl ActionKindGet for RpcAction {
             Self::SnarkerJobCommit(a) => a.kind(),
             Self::SnarkerJobSpec(a) => a.kind(),
             Self::SnarkerWorkersGet(a) => a.kind(),
+            Self::HealthCheck(a) => a.kind(),
             Self::Finish(a) => a.kind(),
         }
     }
@@ -923,6 +925,12 @@ impl ActionKindGet for RpcSnarkerJobSpecAction {
 impl ActionKindGet for RpcSnarkersWorkersGetAction {
     fn kind(&self) -> ActionKind {
         ActionKind::RpcSnarkersWorkersGet
+    }
+}
+
+impl ActionKindGet for RpcHealthCheckAction {
+    fn kind(&self) -> ActionKind {
+        ActionKind::RpcHealthCheck
     }
 }
 

node/src/event_source/event.rs
@@ -43,6 +43,7 @@ impl std::fmt::Display for Event {
                     }
                     RpcRequest::SnarkerJobSpec { job_id } => write!(f, "SnarkerJobSpec, {job_id}"),
                     RpcRequest::SnarkerWorkers => write!(f, "SnarkerWorkers"),
+                    RpcRequest::HealthCheck => write!(f, "HealthCheck"),
                 }
             }
             Self::ExternalSnarkWorker(event) => {

node/src/event_source/event_source_effects.rs
@@ -32,7 +32,7 @@ use crate::rpc::{
     RpcP2pConnectionOutgoingInitAction, RpcRequest, RpcScanStateSummaryGetAction,
     RpcSnarkPoolAvailableJobsGetAction, RpcSnarkPoolJobGetAction, RpcSnarkerConfigGetAction,
     RpcSnarkerJobCommitAction, RpcSnarkerJobSpecAction, RpcSnarkersWorkersGetAction,
-    RpcSyncStatsGetAction,
+    RpcSyncStatsGetAction, RpcHealthCheckAction,
 };
 use crate::snark::block_verify::{SnarkBlockVerifyErrorAction, SnarkBlockVerifySuccessAction};
 use crate::snark::work_verify::{SnarkWorkVerifyErrorAction, SnarkWorkVerifySuccessAction};
@@ -242,6 +242,9 @@ pub fn event_source_effects<S: Service>(store: &mut Store<S>, action: EventSourc
                 RpcRequest::SnarkerWorkers => {
                     store.dispatch(RpcSnarkersWorkersGetAction { rpc_id });
                 }
+                RpcRequest::HealthCheck => {
+                    store.dispatch(RpcHealthCheckAction { rpc_id });
+                },
             },
             Event::ExternalSnarkWorker(e) => match e {
                 ExternalSnarkWorkerEvent::Started => {

node/src/rpc/mod.rs
@@ -53,6 +53,7 @@ pub enum RpcRequest {
     SnarkerJobCommit { job_id: SnarkJobId },
     SnarkerJobSpec { job_id: SnarkJobId },
     SnarkerWorkers,
+    HealthCheck,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -278,3 +279,5 @@ impl From<&MinaBaseUserCommandStableV2> for RpcScanStateSummaryBlockTransactionK
         }
     }
 }
+
+pub type RpcHealthCheckResponse = Result<(), String>;

node/src/rpc/rpc_effects.rs
@@ -361,6 +361,24 @@ pub fn rpc_effects<S: Service>(store: &mut Store<S>, action: RpcActionWithMeta) 
                 return;
             }
         }
+        RpcAction::HealthCheck(action) => {
+            let some_peers = store
+                .state()
+                .p2p
+                .ready_peers_iter()
+                .map(|(peer_id, _peer)| {
+                    openmina_core::log::debug!(openmina_core::log::system_time(); "found ready peer: {peer_id}")
+                })
+                .next()
+                .ok_or(String::from("no ready peers"));
+            if store
+                .service()
+                .respond_health_check(action.rpc_id, some_peers)
+                .is_err()
+            {
+                return;
+            }
+        }
         RpcAction::Finish(_) => {}
     }
 }

commit a96136c65808e3567b1fdaa6e8068b528893de8f

fix(node/p2p): connecting 2 rust nodes failing because rpc channel is being dropped midway
Broke with: https://github.com/openmina/openmina/commit/09a9b7612e9e87fef2a16bba0b0415e104a94020#diff-11d338081a1445e76741181b44fb934e652e1bb53e7097178b4d400dc21f7420L124

node/native/src/rpc.rs
@@ -98,10 +98,22 @@ impl node::rpc::RpcService for NodeService {
         respond_p2p_connection_outgoing,
         RpcP2pConnectionOutgoingResponse
     );
-    rpc_service_impl!(
-        respond_p2p_connection_incoming_answer,
-        P2pConnectionResponse
-    );
+
+    fn respond_p2p_connection_incoming_answer(
+        &mut self,
+        rpc_id: RpcId,
+        response: P2pConnectionResponse,
+    ) -> Result<(), RespondError> {
+        let entry = self.rpc.pending.get(rpc_id);
+        let chan = entry.ok_or(RespondError::UnknownRpcId)?;
+        let chan = chan
+            .downcast_ref::<mpsc::Sender<RpcP2pConnectionIncomingResponse>>()
+            .ok_or(RespondError::UnexpectedResponseType)?
+            .clone();
+        chan.try_send(RpcP2pConnectionIncomingResponse::Answer(response))
+            .or(Err(RespondError::RespondingFailed))?;
+        Ok(())
+    }
 
     fn respond_p2p_connection_incoming(
         &mut self,

node/src/rpc/rpc_actions.rs
@@ -262,14 +262,12 @@ pub struct RpcHealthCheckAction {
 
 impl redux::EnablingCondition<crate::State> for RpcHealthCheckAction {}
 
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RpcReadinessCheckAction {
     pub rpc_id: RpcId,
 }
 
 impl redux::EnablingCondition<crate::State> for RpcReadinessCheckAction {}
-
 
 /// Finish/Cleanup rpc request.
 #[derive(Serialize, Deserialize, Debug, Clone)]