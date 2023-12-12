003f2a994d727a13809fe9fe8cb6034abfd771b4
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2023-12-12 17:52:28 +0300

    test(chores): connection handling tests

---
+++ node/testing/tests/p2p_basic_connections.rs
@@ -0,0 +1,24 @@
+use openmina_node_testing::scenarios::p2p::basic_connection_handling::{
+    AllNodesConnectionsAreSymmetric, MaxNumberOfPeers, SeedConnectionsAreSymmetric,
+    SimultaneousConnections,
+};
+
+mod common;
+
+scenario_test!(
+    simultaneous_connections,
+    SimultaneousConnections,
+    SimultaneousConnections
+);
+scenario_test!(
+    all_nodes_connections_are_symmetric,
+    AllNodesConnectionsAreSymmetric,
+    AllNodesConnectionsAreSymmetric
+);
+scenario_test!(
+    seed_connections_are_symmetric,
+    SeedConnectionsAreSymmetric,
+    SeedConnectionsAreSymmetric
+);
+
+scenario_test!(max_number_of_peers, MaxNumberOfPeers, MaxNumberOfPeers);
