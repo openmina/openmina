20ca347e93824ca3c8942f8bf11b3b79102be5c7
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-12-21 18:04:35 +0100

    feat(replace-libp2p): implement pnet

--- node/testing/src/service/mod.rs
+++ node/testing/src/service/mod.rs
@@ -22,7 +22,7 @@ use node::external_snark_worker::ExternalSnarkWorkerEvent;
 use node::p2p::service_impl::{
     libp2p::Libp2pService, webrtc_with_libp2p::P2pServiceWebrtcWithLibp2p,
 };
-use node::p2p::P2pMioService;
+use node::p2p::{P2pCryptoService, P2pMioService};
 use node::recorder::Recorder;
 use node::service::BlockProducerVrfEvaluatorService;
 use node::snark::block_verify::{
@@ -196,6 +196,12 @@ impl node::Service for NodeTestingService {
 
     fn recorder(&mut self) -> &mut Recorder {
         self.real.recorder()
+    }
+}
+
+impl P2pCryptoService for NodeTestingService {
+    fn generate_random_nonce(&mut self) -> [u8; 24] {
+        self.real.generate_random_nonce()
     }
 }
 
