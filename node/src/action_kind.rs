e37d692024b55d58d2f208e1d760795ec1d9cb2b
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-12-23 15:22:15 +0100

    feat(replace-libp2p): select protocol implement token parsing

--- node/src/action_kind.rs
+++ node/src/action_kind.rs
@@ -40,7 +40,8 @@ use crate::p2p::network::pnet::{
     P2pNetworkPnetSetupNonceAction,
 };
 use crate::p2p::network::select::{
-    P2pNetworkSelectAction, P2pNetworkSelectIncomingDataAction, P2pNetworkSelectInitAction,
+    P2pNetworkSelectAction, P2pNetworkSelectIncomingDataAction,
+    P2pNetworkSelectIncomingTokenAction, P2pNetworkSelectInitAction,
 };
 use crate::p2p::network::P2pNetworkAction;
 use crate::p2p::peer::P2pPeerAction;
@@ -197,6 +198,7 @@ pub enum ActionKind {
     P2pNetworkPnetOutgoingData,
     P2pNetworkPnetSetupNonce,
     P2pNetworkSelectIncomingData,
+    P2pNetworkSelectIncomingToken,
     P2pNetworkSelectInit,
     P2pPeerBestTipUpdate,
     P2pPeerReady,
@@ -312,7 +314,7 @@ pub enum ActionKind {
 }
 
 impl ActionKind {
-    pub const COUNT: u16 = 244;
+    pub const COUNT: u16 = 245;
 }
 
 impl std::fmt::Display for ActionKind {
@@ -878,6 +880,7 @@ impl ActionKindGet for P2pNetworkSelectAction {
         match self {
             Self::Init(a) => a.kind(),
             Self::IncomingData(a) => a.kind(),
+            Self::IncomingToken(a) => a.kind(),
         }
     }
 }
@@ -950,6 +953,12 @@ impl ActionKindGet for P2pNetworkSelectInitAction {
 impl ActionKindGet for P2pNetworkSelectIncomingDataAction {
     fn kind(&self) -> ActionKind {
         ActionKind::P2pNetworkSelectIncomingData
+    }
+}
+
+impl ActionKindGet for P2pNetworkSelectIncomingTokenAction {
+    fn kind(&self) -> ActionKind {
+        ActionKind::P2pNetworkSelectIncomingToken
     }
 }
 
--- p2p/src/network/select/p2p_network_select_state.rs
+++ p2p/src/network/select/p2p_network_select_state.rs
@@ -8,16 +8,18 @@ use super::*;
 #[derive(Default, Serialize, Deserialize, Debug, Clone)]
 pub struct P2pNetworkSelectState {
     pub kind: SelectKind,
-    pub recv: recv::State,
     pub incoming: bool,
 
-    pub handshake_sent: bool,
-    pub handshake_received: bool,
+    pub recv: recv::State,
+    pub tokens: Vec<Vec<u8>>,
+    pub remaining: Vec<u8>,
+
+    pub negotiated: Option<Vec<u8>>,
 }
 
 const NAME: &'static [u8] = b"\x13/multistream/1.0.0\n";
 const SIM: &'static [u8] = b"\x1d/libp2p/simultaneous-connect\n";
-const NA: &'static [u8] = b"\x03na\n";
+// const NA: &'static [u8] = b"\x03na\n";
 
 impl P2pNetworkSelectAction {
     pub fn effects<Store, S>(&self, _meta: &ActionMeta, store: &mut Store)
@@ -25,6 +27,7 @@ impl P2pNetworkSelectAction {
         Store: crate::P2pStore<S>,
         Store::Service: P2pMioService,
         P2pNetworkPnetOutgoingDataAction: redux::EnablingCondition<S>,
+        P2pNetworkSelectIncomingTokenAction: redux::EnablingCondition<S>,
     {
         let kind = self.select_kind();
         let Some(state) = store
@@ -47,27 +50,46 @@ impl P2pNetworkSelectAction {
         match self {
             Self::Init(a) => match kind {
                 SelectKind::Authentication => {
-                    store.dispatch(P2pNetworkPnetOutgoingDataAction {
-                        addr: a.addr,
-                        data: NAME.to_vec().into_boxed_slice(),
-                    });
-                    store.dispatch(P2pNetworkPnetOutgoingDataAction {
-                        addr: a.addr,
-                        data: SIM.to_vec().into_boxed_slice(),
-                    });
-                    store.dispatch(P2pNetworkPnetOutgoingDataAction {
-                        addr: a.addr,
-                        data: kind.supported()[0].to_vec().into_boxed_slice(),
-                    });
+                    if state.incoming {
+                        store.dispatch(P2pNetworkPnetOutgoingDataAction {
+                            addr: a.addr,
+                            data: NAME.to_vec().into_boxed_slice(),
+                        });
+                    } else {
+                        let mut data = NAME.to_vec();
+                        data.extend_from_slice(SIM);
+                        data.extend_from_slice(kind.supported()[0]);
+                        store.dispatch(P2pNetworkPnetOutgoingDataAction {
+                            addr: a.addr,
+                            data: data.into_boxed_slice(),
+                        });
+                    }
                 }
                 _ => unimplemented!(),
             },
             Self::IncomingData(a) => {
-                dbg!(std::str::from_utf8(&a.data[1..])).unwrap_or_default();
+                let tokens = state
+                    .tokens
+                    .iter()
+                    .map(|token| token.clone().into_boxed_slice())
+                    .collect::<Vec<_>>();
+                for token in tokens {
+                    store.dispatch(P2pNetworkSelectIncomingTokenAction {
+                        addr: a.addr,
+                        peer_id: a.peer_id,
+                        stream_id: a.stream_id,
+                        token,
+                    });
+                }
+            }
+            Self::IncomingToken(a) => {
+                if a.token.as_ref() == NAME {
+                    // do nothing, it is handshake token
+                } else {
+                    dbg!(std::str::from_utf8(&a.token)).unwrap_or_default();
+                }
             }
         }
-
-        let _ = store;
     }
 }
 
@@ -80,7 +102,26 @@ impl P2pNetworkSelectState {
                 self.incoming = a.incoming;
             }
             P2pNetworkSelectAction::IncomingData(a) => {
-                let _ = a;
+                self.remaining.extend_from_slice(&a.data);
+                if let Some(negotiated) = &self.negotiated {
+                    // TODO: send to the negotiated handler
+                    let _ = negotiated;
+                } else {
+                    loop {
+                        let (token, remaining) = self.recv.parse_protocol(&self.remaining);
+                        self.remaining = remaining.to_vec();
+                        match token {
+                            Some(v) => {
+                                self.tokens.push(v.to_vec());
+                                self.recv.consume();
+                            }
+                            None => break,
+                        }
+                    }
+                }
+            }
+            P2pNetworkSelectAction::IncomingToken(_) => {
+                self.tokens.remove(0);
             }
         }
     }
