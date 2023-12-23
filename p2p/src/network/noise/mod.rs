b4a2060e7856d784b726933fcac3978c47bde759
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-12-23 21:28:11 +0100

    feat(replace-libp2p): start noise

---
+++ p2p/src/network/noise/mod.rs
@@ -0,0 +1,5 @@
+mod p2p_network_noise_actions;
+pub use self::p2p_network_noise_actions::*;
+
+mod p2p_network_noise_state;
+pub use self::p2p_network_noise_state::P2pNetworkNoiseState;
--- p2p/src/network/p2p_network_actions.rs
+++ p2p/src/network/p2p_network_actions.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::{connection::*, pnet::*, select::*};
+use super::{connection::*, noise::*, pnet::*, select::*};
 
 use crate::P2pState;
 
@@ -9,6 +9,7 @@ pub enum P2pNetworkAction {
     Connection(P2pNetworkConnectionAction),
     Pnet(P2pNetworkPnetAction),
     Select(P2pNetworkSelectAction),
+    Noise(P2pNetworkNoiseAction),
 }
 
 impl redux::EnablingCondition<P2pState> for P2pNetworkAction {
@@ -17,6 +18,7 @@ impl redux::EnablingCondition<P2pState> for P2pNetworkAction {
             Self::Connection(v) => v.is_enabled(state),
             Self::Pnet(v) => v.is_enabled(state),
             Self::Select(v) => v.is_enabled(state),
+            Self::Noise(v) => v.is_enabled(state),
         }
     }
 }
