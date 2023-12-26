cf6cc83eba3381fed7ff0a4a3c38cc8c9f7e4faf
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-12-26 19:55:22 +0100

    feat(replace-libp2p): implement yamux frame parsing

--- node/src/p2p/network/mod.rs
+++ node/src/p2p/network/mod.rs
@@ -137,3 +137,9 @@ impl redux::EnablingCondition<crate::State> for P2pNetworkYamuxOutgoingDataActio
         self.is_enabled(&state.p2p)
     }
 }
+
+impl redux::EnablingCondition<crate::State> for P2pNetworkYamuxIncomingFrameAction {
+    fn is_enabled(&self, state: &crate::State) -> bool {
+        self.is_enabled(&state.p2p)
+    }
+}
