85dae6e1fe2c61d79cbb5517a7cf7aa502937e7e
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2023-12-26 19:55:22 +0100

    feat(replace-libp2p): implement yamux frame parsing

--- p2p/src/network/yamux/p2p_network_yamux_actions.rs
+++ p2p/src/network/yamux/p2p_network_yamux_actions.rs
@@ -2,12 +2,14 @@ use std::net::SocketAddr;
 
 use serde::{Deserialize, Serialize};
 
+use super::p2p_network_yamux_state::YamuxFrame;
 use crate::{Data, P2pNetworkAction, P2pState};
 
 #[derive(derive_more::From, Serialize, Deserialize, Debug, Clone)]
 pub enum P2pNetworkYamuxAction {
     IncomingData(P2pNetworkYamuxIncomingDataAction),
     OutgoingData(P2pNetworkYamuxOutgoingDataAction),
+    IncomingFrame(P2pNetworkYamuxIncomingFrameAction),
 }
 
 impl P2pNetworkYamuxAction {
@@ -15,6 +17,7 @@ impl P2pNetworkYamuxAction {
         match self {
             Self::IncomingData(a) => a.addr,
             Self::OutgoingData(a) => a.addr,
+            Self::IncomingFrame(a) => a.addr,
         }
     }
 }
@@ -32,6 +35,12 @@ pub struct P2pNetworkYamuxOutgoingDataAction {
     pub data: Data,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct P2pNetworkYamuxIncomingFrameAction {
+    pub addr: SocketAddr,
+    pub frame: YamuxFrame,
+}
+
 impl From<P2pNetworkYamuxIncomingDataAction> for crate::P2pAction {
     fn from(a: P2pNetworkYamuxIncomingDataAction) -> Self {
         Self::Network(P2pNetworkAction::Yamux(a.into()))
@@ -44,11 +53,18 @@ impl From<P2pNetworkYamuxOutgoingDataAction> for crate::P2pAction {
     }
 }
 
+impl From<P2pNetworkYamuxIncomingFrameAction> for crate::P2pAction {
+    fn from(a: P2pNetworkYamuxIncomingFrameAction) -> Self {
+        Self::Network(P2pNetworkAction::Yamux(a.into()))
+    }
+}
+
 impl redux::EnablingCondition<P2pState> for P2pNetworkYamuxAction {
     fn is_enabled(&self, state: &P2pState) -> bool {
         match self {
             Self::IncomingData(v) => v.is_enabled(state),
             Self::OutgoingData(v) => v.is_enabled(state),
+            Self::IncomingFrame(v) => v.is_enabled(state),
         }
     }
 }
@@ -60,6 +76,12 @@ impl redux::EnablingCondition<P2pState> for P2pNetworkYamuxIncomingDataAction {
 }
 
 impl redux::EnablingCondition<P2pState> for P2pNetworkYamuxOutgoingDataAction {
+    fn is_enabled(&self, _state: &P2pState) -> bool {
+        true
+    }
+}
+
+impl redux::EnablingCondition<P2pState> for P2pNetworkYamuxIncomingFrameAction {
     fn is_enabled(&self, _state: &P2pState) -> bool {
         true
     }
