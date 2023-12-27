96432d3b88d59edde2d8f8b31eec18e5312e4231
Zura Benashvili <zura.benashvili@gmail.com>
2023-12-28 01:33:24 +0400

    test(scenarios): prepare 4 block producers re: #176

--- node/testing/src/node/config.rs
+++ node/testing/src/node/config.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 pub use super::ocaml::{DaemonJson, OcamlNodeConfig, OcamlNodeExecutable, OcamlNodeTestingConfig};
-pub use super::rust::{RustNodeTestingConfig, TestPeerId};
+pub use super::rust::{RustNodeBlockProducerTestingConfig, RustNodeTestingConfig, TestPeerId};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(tag = "kind")]
--- node/testing/src/scenarios/multi_node/mod.rs
+++ node/testing/src/scenarios/multi_node/mod.rs
@@ -1,3 +1,5 @@
+pub mod sync_4_block_producers;
+
 pub mod basic_connectivity_initial_joining;
 pub mod basic_connectivity_peer_discovery;
 
