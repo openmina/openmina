6cafc39aa881dfe8bab593dc29d6adcef72a46d3
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2024-02-01 11:00:42 +0300

    chores(new-libp2p): better error handling

--- node/src/logger/logger_effects.rs
+++ node/src/logger/logger_effects.rs
@@ -543,14 +543,16 @@ pub fn logger_effects<S: Service>(store: &Store<S>, action: ActionWithMetaRef<'_
                             openmina_core::log::error!(
                                 meta.time();
                                 kind = kind.to_string(),
-                                summary = format!("failed select authentication on addr: {}", action.addr),
+                                error = action.error,
+                                summary = format!("failed select authentication on addr {}: {}", action.addr, action.error),
                             )
                         }
                         SelectKind::Multiplexing(peer_id) => {
                             openmina_core::log::error!(
                                 meta.time();
                                 kind = kind.to_string(),
-                                summary = format!("failed select multiplexing on addr: {}", action.addr),
+                                error = action.error,
+                                summary = format!("failed select multiplexing on addr {}: {}", action.addr, action.error),
                                 peer_id = peer_id.to_string(),
                             )
                         }
@@ -558,7 +560,8 @@ pub fn logger_effects<S: Service>(store: &Store<S>, action: ActionWithMetaRef<'_
                             openmina_core::log::error!(
                                 meta.time();
                                 kind = kind.to_string(),
-                                summary = format!("failed select stream on addr: {}", action.addr),
+                                error = action.error,
+                                summary = format!("failed select stream on addr {}: {}", action.addr, action.error),
                                 peer_id = peer_id.to_string(),
                                 stream_id = stream_id,
                             )
@@ -722,6 +725,9 @@ pub fn logger_effects<S: Service>(store: &Store<S>, action: ActionWithMetaRef<'_
                     }
                     _ => {}
                 },
+                P2pNetworkAction::Kad(_) => {
+                    //
+                }
                 P2pNetworkAction::Rpc(action) => match action {
                     P2pNetworkRpcAction::Init(action) => {
                         openmina_core::log::info!(
