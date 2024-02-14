b5cb8ddbf483b4d6c9ef033526e7e276f4f560f0
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2024-02-14 12:45:01 +0100

    feat(bootstrap-sandboc): added recorder/replayer

---
+++ tools/bootstrap-sandbox/src/replay.rs
@@ -0,0 +1,222 @@
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fs::{self, File},
+    path::Path,
+};
+
+use binprot::BinProtRead;
+use libp2p::{futures::StreamExt, swarm::SwarmEvent};
+use libp2p_rpc_behaviour::{Behaviour, Event, Received};
+use mina_p2p_messages::{
+    rpc::{
+        AnswerSyncLedgerQueryV2, GetAncestryV2, GetBestTipV2,
+        GetStagedLedgerAuxAndPendingCoinbasesAtHashV2, GetTransitionChainProofV1ForV2,
+        GetTransitionChainV2,
+    },
+    rpc_kernel::{QueryHeader, QueryPayload, RpcMethod, RpcResult},
+    v2,
+};
+
+use super::snarked_ledger::SnarkedLedger;
+
+pub async fn run(mut swarm: libp2p::Swarm<Behaviour>, path_main: &Path, height: u32) {
+    let path_blocks = path_main.join("blocks");
+    let path = path_main.join(height.to_string());
+
+    let mut file = File::open(path.join("best_tip")).unwrap();
+    let best_tip = <GetBestTipV2 as RpcMethod>::Response::binprot_read(&mut file).unwrap();
+
+    let mut file = File::open(path.join("ancestry")).unwrap();
+    let ancestry = <GetAncestryV2 as RpcMethod>::Response::binprot_read(&mut file).unwrap();
+
+    let mut file = File::open(path.join("staged_ledger_aux")).unwrap();
+    type T = GetStagedLedgerAuxAndPendingCoinbasesAtHashV2;
+    let staged_ledger_aux = <T as RpcMethod>::Response::binprot_read(&mut file).unwrap();
+
+    let mut ledgers = BTreeMap::new();
+    for entry in fs::read_dir(path.join("ledgers")).unwrap() {
+        let entry = entry.unwrap();
+        let file = File::open(entry.path()).unwrap();
+        let ledger = SnarkedLedger::load_bin(file).unwrap();
+        ledgers.insert(entry.file_name().to_str().unwrap().to_string(), ledger);
+    }
+
+    let file = File::open(path_main.join("blocks").join("table.json")).unwrap();
+    let table = serde_json::from_reader::<_, BTreeMap<String, u32>>(file).unwrap();
+
+    let mut peers = BTreeSet::default();
+
+    while let Some(event) = swarm.next().await {
+        match event {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                log::info!("listen on {address}");
+            }
+            SwarmEvent::ConnectionEstablished {
+                peer_id, endpoint, ..
+            } => {
+                log::info!(
+                    "new connection {peer_id}, {}",
+                    endpoint.get_remote_address()
+                );
+            }
+            SwarmEvent::Behaviour((peer_id, Event::ConnectionEstablished)) => {
+                peers.insert(peer_id);
+                log::info!("new connection {peer_id}");
+            }
+            SwarmEvent::Behaviour((peer_id, Event::ConnectionClosed)) => {
+                log::info!("connection closed {peer_id}");
+                peers.remove(&peer_id);
+            }
+            SwarmEvent::Behaviour((
+                peer_id,
+                Event::Stream {
+                    stream_id,
+                    received,
+                },
+            )) => match received {
+                Received::HandshakeDone => {
+                    log::info!("new stream {peer_id} {stream_id:?}");
+                }
+                Received::Menu(menu) => {
+                    log::info!("menu: {menu:?}");
+                }
+                Received::Query {
+                    header: QueryHeader { tag, version, id },
+                    bytes,
+                } => {
+                    let mut bytes = bytes.as_slice();
+                    let tag = std::str::from_utf8(tag.as_ref()).unwrap();
+                    log::info!("handling {tag}, {}", version);
+                    match (tag, version) {
+                        (GetBestTipV2::NAME, GetBestTipV2::VERSION) => {
+                            swarm
+                                .behaviour_mut()
+                                .respond::<GetBestTipV2>(
+                                    peer_id,
+                                    stream_id,
+                                    id,
+                                    Ok(best_tip.clone()),
+                                )
+                                .unwrap();
+                        }
+                        (GetAncestryV2::NAME, GetAncestryV2::VERSION) => {
+                            swarm
+                                .behaviour_mut()
+                                .respond::<GetAncestryV2>(
+                                    peer_id,
+                                    stream_id,
+                                    id,
+                                    Ok(ancestry.clone()),
+                                )
+                                .unwrap();
+                        }
+                        (AnswerSyncLedgerQueryV2::NAME, AnswerSyncLedgerQueryV2::VERSION) => {
+                            type T = AnswerSyncLedgerQueryV2;
+                            let (hash, query) =
+                                QueryPayload::<<T as RpcMethod>::Query>::binprot_read(&mut bytes)
+                                    .unwrap()
+                                    .0;
+
+                            let hash = v2::LedgerHash::from(v2::MinaBaseLedgerHash0StableV1(hash));
+                            let hash_str = match serde_json::to_value(&hash).unwrap() {
+                                serde_json::Value::String(s) => s,
+                                _ => panic!(),
+                            };
+
+                            let ledger = ledgers.get_mut(&hash_str).unwrap();
+                            let response = ledger.serve_query(query);
+
+                            swarm
+                                .behaviour_mut()
+                                .respond::<T>(peer_id, stream_id, id, Ok(RpcResult(Ok(response))))
+                                .unwrap();
+                        }
+                        (
+                            GetStagedLedgerAuxAndPendingCoinbasesAtHashV2::NAME,
+                            GetStagedLedgerAuxAndPendingCoinbasesAtHashV2::VERSION,
+                        ) => swarm
+                            .behaviour_mut()
+                            .respond::<GetStagedLedgerAuxAndPendingCoinbasesAtHashV2>(
+                                peer_id,
+                                stream_id,
+                                id,
+                                Ok(staged_ledger_aux.clone()),
+                            )
+                            .unwrap(),
+                        (GetTransitionChainV2::NAME, GetTransitionChainV2::VERSION) => {
+                            type T = GetTransitionChainV2;
+                            let hashes =
+                                QueryPayload::<<T as RpcMethod>::Query>::binprot_read(&mut bytes)
+                                    .unwrap()
+                                    .0;
+
+                            // let mut contains_last = false;
+                            let response = hashes
+                                .into_iter()
+                                .map(|hash| {
+                                    let hash =
+                                        v2::StateHash::from(v2::DataHashLibStateHashStableV1(hash));
+                                    // if hash
+                                    //     == best_tip
+                                    //         .as_ref()
+                                    //         .unwrap()
+                                    //         .data
+                                    //         .header
+                                    //         .protocol_state
+                                    //         .previous_state_hash
+                                    // {
+                                    //     contains_last = true;
+                                    // }
+                                    let height = table.get(&hash.to_string()).unwrap();
+                                    let path =
+                                        path_blocks.join(height.to_string()).join(hash.to_string());
+                                    let mut file = File::open(path).unwrap();
+                                    binprot::BinProtRead::binprot_read(&mut file).unwrap()
+                                })
+                                .collect();
+                            swarm
+                                .behaviour_mut()
+                                .respond::<T>(peer_id, stream_id, id, Ok(Some(response)))
+                                .unwrap();
+                            // if contains_last {
+                            //     swarm.disconnect_peer_id(peer_id).unwrap();
+                            // }
+                        }
+                        (
+                            GetTransitionChainProofV1ForV2::NAME,
+                            GetTransitionChainProofV1ForV2::VERSION,
+                        ) => {
+                            type T = GetTransitionChainProofV1ForV2;
+                            let hash =
+                                QueryPayload::<<T as RpcMethod>::Query>::binprot_read(&mut bytes)
+                                    .unwrap()
+                                    .0;
+
+                            let hash = v2::StateHash::from(v2::DataHashLibStateHashStableV1(hash));
+                            let response = if let Some(height) = table.get(&hash.to_string()) {
+                                let path = path_blocks
+                                    .join(height.to_string())
+                                    .join(format!("proof_{hash}"));
+                                let mut file = File::open(path).unwrap();
+                                binprot::BinProtRead::binprot_read(&mut file).unwrap()
+                            } else {
+                                log::warn!("no proof for block {hash}");
+                                None
+                            };
+
+                            swarm
+                                .behaviour_mut()
+                                .respond::<T>(peer_id, stream_id, id, Ok(response))
+                                .unwrap();
+                        }
+                        (name, version) => {
+                            log::warn!("TODO: unhandled {name}, {version}");
+                        }
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
---
+++ tools/bootstrap-sandbox/src/snarked_ledger.rs
@@ -0,0 +1,248 @@
+use binprot::{BinProtRead, BinProtWrite};
+use std::{future::Future, io, pin::Pin};
+use thiserror::Error;
+
+use ledger::{Account, AccountIndex, Address, BaseLedger, Database, Mask};
+use mina_p2p_messages::{core::Info, list::List, rpc::AnswerSyncLedgerQueryV2, v2};
+
+use super::client::Client;
+
+pub struct SnarkedLedger {
+    pub inner: Mask,
+    // NOTE: it is not the same as the merkle tree root
+    pub top_hash: Option<v2::LedgerHash>,
+    pub num: u32,
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("{0}")]
+    Serde(#[from] serde_json::Error),
+    #[error("{0}")]
+    Io(#[from] io::Error),
+}
+
+impl SnarkedLedger {
+    pub fn empty() -> Self {
+        SnarkedLedger {
+            inner: Mask::new_root(Database::create(35)),
+            top_hash: None,
+            num: 0,
+        }
+    }
+
+    // for debugging
+    pub fn store_bin<W>(&self, mut writer: W) -> io::Result<()>
+    where
+        W: io::Write,
+    {
+        let accounts = self.inner.fold(vec![], |mut accounts, account| {
+            accounts.push(account.clone());
+            accounts
+        });
+        self.top_hash.binprot_write(&mut writer)?;
+        accounts.binprot_write(&mut writer)
+    }
+
+    pub fn load_bin<R>(mut reader: R) -> Result<Self, binprot::Error>
+    where
+        R: io::Read,
+    {
+        let top_hash = Option::binprot_read(&mut reader)?;
+        let accounts = Vec::<Account>::binprot_read(&mut reader)?;
+
+        let num = accounts.len() as _;
+        let mut inner = Mask::new_root(Database::create(35));
+        for account in accounts {
+            let account_id = account.id();
+            inner.get_or_create_account(account_id, account).unwrap();
+        }
+
+        let _ = inner.merkle_root();
+
+        Ok(SnarkedLedger {
+            inner,
+            top_hash,
+            num,
+        })
+    }
+
+    pub async fn sync_new(&mut self, client: &mut Client, root: &v2::LedgerHash) {
+        let q = v2::MinaLedgerSyncLedgerQueryStableV1::NumAccounts;
+        let r = client
+            .rpc::<AnswerSyncLedgerQueryV2>((root.0.clone(), q))
+            .await
+            .unwrap()
+            .0
+            .unwrap();
+        let (num, hash) = match r {
+            v2::MinaLedgerSyncLedgerAnswerStableV2::NumAccounts(num, hash) => (num.0, hash),
+            _ => panic!(),
+        };
+        self.top_hash = Some(hash.clone());
+        self.num = num as _;
+
+        if self.inner.num_accounts() > num as _ {
+            self.inner = Mask::new_root(Database::create(35));
+        }
+
+        self.sync_at_depth_new(client, root.clone(), hash.clone(), 0, 0)
+            .await;
+        let actual_hash = self.inner.merkle_root();
+        let actual_hash = v2::LedgerHash::from(v2::MinaBaseLedgerHash0StableV1(actual_hash.into()));
+        assert_eq!(actual_hash, root.clone());
+    }
+
+    fn sync_at_depth_boxed_new<'a, 'b: 'a>(
+        &'b mut self,
+        client: &'a mut Client,
+        root: v2::LedgerHash,
+        hash: v2::LedgerHash,
+        depth: i32,
+        pos: u32,
+    ) -> Pin<Box<dyn Future<Output = ()> + 'a>> {
+        Box::pin(self.sync_at_depth_new(client, root, hash, depth, pos))
+    }
+
+    async fn sync_at_depth_new(
+        &mut self,
+        client: &mut Client,
+        root: v2::LedgerHash,
+        hash: v2::LedgerHash,
+        depth: i32,
+        pos: u32,
+    ) {
+        let addr = Address::from_index(AccountIndex(pos as _), depth as _);
+        let actual_hash = self.inner.get_inner_hash_at_addr(addr.clone()).unwrap();
+        if depth == 0 && root.0 == actual_hash.into() || depth > 0 && hash.0 == actual_hash.into() {
+            return;
+        }
+
+        if depth == 32 {
+            let p = pos.to_be_bytes().to_vec();
+            let q = v2::MinaLedgerSyncLedgerQueryStableV1::WhatContents(
+                v2::MerkleAddressBinableArgStableV1((depth as u64).into(), p.into()),
+            );
+            log::info!("{}", serde_json::to_string(&q).unwrap());
+            let r = client
+                .rpc::<AnswerSyncLedgerQueryV2>((root.0.clone(), q))
+                .await
+                .unwrap()
+                .0;
+            match r {
+                Err(Info::CouldNotConstruct(s)) => {
+                    log::error!(
+                        "num: {}, could not construct {}",
+                        self.num,
+                        s.to_string_lossy()
+                    );
+                }
+                Ok(v2::MinaLedgerSyncLedgerAnswerStableV2::ContentsAre(accounts)) => {
+                    for (o, account) in accounts.into_iter().enumerate() {
+                        let account = Account::from(&account);
+                        self.inner
+                            .set_at_index(
+                                AccountIndex((pos * 8) as u64 + o as u64),
+                                Box::new(account),
+                            )
+                            .unwrap();
+                    }
+                }
+                _ => panic!(),
+            }
+        } else {
+            let b = ((depth as usize + 7) / 8).min(4);
+            let p = pos * (1 << (32 - depth));
+            let p = p.to_be_bytes()[..b].to_vec();
+            let q = v2::MinaLedgerSyncLedgerQueryStableV1::WhatChildHashes(
+                v2::MerkleAddressBinableArgStableV1((depth as u64).into(), p.into()),
+            );
+            log::info!("{}", serde_json::to_string(&q).unwrap());
+            let r = client
+                .rpc::<AnswerSyncLedgerQueryV2>((root.0.clone(), q))
+                .await
+                .unwrap()
+                .0
+                .unwrap();
+            match r {
+                v2::MinaLedgerSyncLedgerAnswerStableV2::ChildHashesAre(l, r) => {
+                    self.sync_at_depth_boxed_new(client, root.clone(), l, depth + 1, pos * 2)
+                        .await;
+                    self.sync_at_depth_boxed_new(client, root.clone(), r, depth + 1, pos * 2 + 1)
+                        .await;
+                }
+                _ => panic!(),
+            };
+        }
+
+        let addr = Address::from_index(AccountIndex(pos as _), depth as _);
+        let actual_hash = self.inner.get_inner_hash_at_addr(addr).unwrap();
+        let actual_hash = v2::LedgerHash::from(v2::MinaBaseLedgerHash0StableV1(actual_hash.into()));
+        if depth == 0 {
+            assert_eq!(root, actual_hash);
+        } else {
+            assert_eq!(hash, actual_hash);
+        }
+    }
+
+    pub fn serve_query(
+        &mut self,
+        q: v2::MinaLedgerSyncLedgerQueryStableV1,
+    ) -> v2::MinaLedgerSyncLedgerAnswerStableV2 {
+        log::info!("query: {q:?}");
+        match q {
+            v2::MinaLedgerSyncLedgerQueryStableV1::NumAccounts => {
+                v2::MinaLedgerSyncLedgerAnswerStableV2::NumAccounts(
+                    (self.num as u64).into(),
+                    self.top_hash.as_ref().unwrap().clone(),
+                )
+            }
+            v2::MinaLedgerSyncLedgerQueryStableV1::WhatChildHashes(address) => {
+                let addr = Address::from(address);
+
+                let hash = self
+                    .inner
+                    .get_inner_hash_at_addr(addr.child_left())
+                    .unwrap();
+                let left = v2::LedgerHash::from(v2::MinaBaseLedgerHash0StableV1(hash.into()));
+
+                let hash = self
+                    .inner
+                    .get_inner_hash_at_addr(addr.child_right())
+                    .unwrap();
+                let right = v2::LedgerHash::from(v2::MinaBaseLedgerHash0StableV1(hash.into()));
+
+                v2::MinaLedgerSyncLedgerAnswerStableV2::ChildHashesAre(left, right)
+            }
+            v2::MinaLedgerSyncLedgerQueryStableV1::WhatContents(address) => {
+                let addr = Address::from(address);
+
+                let depth = addr.length();
+                let pos = addr.to_index().0;
+
+                let mut accounts = List::new();
+                let mut offset = 0;
+                let batch_length = 1u64 << (35 - depth);
+                loop {
+                    if offset == batch_length {
+                        break;
+                    }
+
+                    let pos = pos * batch_length + offset;
+                    offset += 1;
+                    if pos == self.num as u64 {
+                        break;
+                    }
+                    let addr = Address::from_index(AccountIndex(pos as _), 35);
+                    let account = self.inner.get(addr);
+                    if let Some(account) = account {
+                        accounts.push_back((&*account).into());
+                    } else {
+                        break;
+                    }
+                }
+                v2::MinaLedgerSyncLedgerAnswerStableV2::ContentsAre(accounts)
+            }
+        }
+    }
+}
