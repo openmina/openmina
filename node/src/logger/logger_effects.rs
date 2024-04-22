2b8994821919a4586d743a8b81963e1a6ee29d08
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2024-04-22 13:23:30 +0300

    feat(p2p-testing): decouple action id from Action

--- node/src/logger/logger_effects.rs
+++ node/src/logger/logger_effects.rs
@@ -8,22 +8,20 @@ use crate::p2p::connection::P2pConnectionAction;
 use crate::p2p::network::P2pNetworkAction;
 use crate::p2p::P2pAction;
 use crate::snark::SnarkAction;
-use crate::{Action, ActionKind, ActionWithMetaRef, Service, Store};
+use crate::{Action, ActionWithMetaRef, Service, Store};
 
 struct ActionLoggerContext {
     time: redux::Timestamp,
     time_str: String,
     node_id: DisplayValue<PeerId>,
-    kind: DisplayValue<ActionKind>,
 }
 
 impl ActionLoggerContext {
-    fn new(time: redux::Timestamp, node_id: PeerId, kind: ActionKind) -> Self {
+    fn new(time: redux::Timestamp, node_id: PeerId) -> Self {
         ActionLoggerContext {
             time,
             time_str: time_to_str(time),
             node_id: display(node_id),
-            kind: display(kind),
         }
     }
 }
@@ -40,15 +38,11 @@ impl EventContext for ActionLoggerContext {
     fn node_id(&self) -> &'_ dyn Value {
         &self.node_id
     }
-
-    fn kind(&self) -> &'_ dyn Value {
-        &self.kind
-    }
 }
 
 pub fn logger_effects<S: Service>(store: &Store<S>, action: ActionWithMetaRef<'_>) {
     let (action, meta) = action.split();
-    let context = ActionLoggerContext::new(meta.time(), store.state().p2p.my_id(), action.kind());
+    let context = ActionLoggerContext::new(meta.time(), store.state().p2p.my_id());
 
     match action {
         Action::P2p(action) => match action {
