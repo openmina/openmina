797f0fe6818bd14f0e261392aa4fea780c779cbb
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2024-04-30 18:54:41 +0200

    fix(pubsub): send outgoing messages to outgoing stream

--- p2p/src/network/pubsub/p2p_network_pubsub_state.rs
+++ p2p/src/network/pubsub/p2p_network_pubsub_state.rs
@@ -19,7 +19,7 @@ pub struct P2pNetworkPubsubState {
 pub struct P2pNetworkPubsubClientState {
     pub protocol: BroadcastAlgorithm,
     pub addr: SocketAddr,
-    pub stream_id: StreamId,
+    pub outgoing_stream_id: Option<StreamId>,
     pub topics: BTreeSet<String>,
     pub message: pb::Rpc,
 }
