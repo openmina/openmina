ad495b9ee55c0f11ceee0d02138c363eea375089
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2024-05-10 14:38:35 +0200

    feat(p2p/pubsub): fix test for block production

--- node/testing/src/scenarios/block_production/basic.rs
+++ node/testing/src/scenarios/block_production/basic.rs
@@ -29,7 +29,10 @@ impl BlockProductionBasic {
         let mut accounts = Vec::with_capacity(genesis_mask.num_accounts());
         genesis_mask.iter(|acc| {
             let pub_key = acc.public_key.into_address();
-            let delegate = acc.delegate.as_ref().map_or(pub_key.clone(), |pk| pk.into_address());
+            let delegate = acc
+                .delegate
+                .as_ref()
+                .map_or(pub_key.clone(), |pk| pk.into_address());
             let acc = serde_json::json!({
                 "pk": pub_key,
                 "balance": acc.balance.to_mina_str(),
@@ -64,38 +67,64 @@ impl BlockProductionBasic {
             .expect("ocaml node didn't become ready");
         let (chain_id, ocaml_node_peer_id) = {
             let node = runner.ocaml_node(ocaml_node).unwrap();
-            let chain_id = node.chain_id().expect("failed to get chain_id from ocaml node");
+            let chain_id = node
+                .chain_id()
+                .expect("failed to get chain_id from ocaml node");
             (chain_id, node.peer_id())
         };
 
-        let seed_cfg = RustNodeTestingConfig{
+        let seed_cfg = RustNodeTestingConfig {
             chain_id: chain_id.into_bytes(),
             initial_time,
             genesis: genesis_cfg.into(),
-            initial_peers: vec![ ListenerNode::Ocaml(ocaml_node)],
+            initial_peers: vec![ListenerNode::Ocaml(ocaml_node)],
             ..RustNodeTestingConfig::berkeley_default()
         };
         let seed_node = runner.add_rust_node(seed_cfg.clone());
 
-        runner.run(RunCfg::default().timeout(Duration::from_secs(60)).action_handler(move |id, _, _, action| {
-            id == seed_node &&
-            matches!(action.action().kind(), ActionKind::TransitionFrontierGenesisInject)
-        })).await.expect("seed node didn't produce genesis block");
+        runner
+            .run(
+                RunCfg::default()
+                    .timeout(Duration::from_secs(60))
+                    .action_handler(move |id, _, _, action| {
+                        id == seed_node
+                            && matches!(
+                                action.action().kind(),
+                                ActionKind::TransitionFrontierGenesisInject
+                            )
+                    }),
+            )
+            .await
+            .expect("seed node didn't produce genesis block");
 
-        runner.run(RunCfg::default().timeout(Duration::from_secs(60)).action_handler(move |id, state, _, action| {
-            dbg!(action.action().kind());
-            id == seed_node && 
-                state.p2p.get_ready_peer(&ocaml_node_peer_id).map_or(false, |peer| peer.best_tip.is_some())
-        })).await.expect("seed node didn't produce genesis block");
+        runner
+            .run(
+                RunCfg::default()
+                    .timeout(Duration::from_secs(60))
+                    .action_handler(move |id, state, _, action| {
+                        dbg!(action.action().kind());
+                        id == seed_node
+                            && state
+                                .p2p
+                                .get_ready_peer(&ocaml_node_peer_id)
+                                .map_or(false, |peer| peer.best_tip.is_some())
+                    }),
+            )
+            .await
+            .expect("seed node didn't produce genesis block");
 
         // make sure genesis blocks are the same.
-        self.assert_all_nodes_best_tip_eq(&runner);
+        self.assert_all_nodes_best_tip_eq(&runner).await;
     }
 
     async fn assert_all_nodes_best_tip_eq(&self, runner: &ClusterRunner<'_>) {
         eprintln!("[check_tips]");
         for (id, node) in runner.ocaml_nodes_iter() {
-            let tip =node.synced_best_tip_async().await.expect("failed to get best tip from ocaml node").expect("ocaml node not synced");
+            let tip = node
+                .synced_best_tip_async()
+                .await
+                .expect("failed to get best tip from ocaml node")
+                .expect("ocaml node not synced");
             eprintln!("{id:?} - {tip}");
         }
         for (id, node) in runner.nodes_iter() {
