41117256171e04b7a1db8a206dfb28d45665a546
Sebastien Chapuis <sebastien@chapu.is>
2024-05-15 14:42:38 +0200

    Use new state machine reducer
    This removes the method `effects`

--- node/src/action_kind.rs
+++ node/src/action_kind.rs
@@ -58,7 +58,7 @@ use crate::snark::work_verify_effectful::SnarkWorkVerifyEffectfulAction;
 use crate::snark::SnarkAction;
 use crate::snark_pool::candidate::SnarkPoolCandidateAction;
 use crate::snark_pool::{SnarkPoolAction, SnarkPoolEffectfulAction};
-use crate::transaction_pool::TransactionPoolAction;
+use crate::transaction_pool::{TransactionPoolAction, TransactionPoolEffectfulAction};
 use crate::transition_frontier::genesis::TransitionFrontierGenesisAction;
 use crate::transition_frontier::genesis_effectful::TransitionFrontierGenesisEffectfulAction;
 use crate::transition_frontier::sync::ledger::snarked::TransitionFrontierSyncLedgerSnarkedAction;
@@ -383,6 +383,7 @@ pub enum ActionKind {
     TransactionPoolBestTipChanged,
     TransactionPoolBestTipChangedWithAccounts,
     TransactionPoolRebroadcast,
+    TransactionPoolEffectfulFetchAccounts,
     TransitionFrontierGenesisInject,
     TransitionFrontierSynced,
     TransitionFrontierGenesisLedgerLoadInit,
@@ -471,7 +472,7 @@ pub enum ActionKind {
 }
 
 impl ActionKind {
-    pub const COUNT: u16 = 391;
+    pub const COUNT: u16 = 392;
 }
 
 impl std::fmt::Display for ActionKind {
@@ -493,6 +494,7 @@ impl ActionKindGet for Action {
             Self::SnarkPool(a) => a.kind(),
             Self::SnarkPoolEffect(a) => a.kind(),
             Self::TransactionPool(a) => a.kind(),
+            Self::TransactionPoolEffect(a) => a.kind(),
             Self::ExternalSnarkWorker(a) => a.kind(),
             Self::BlockProducer(a) => a.kind(),
             Self::Rpc(a) => a.kind(),
@@ -629,6 +631,14 @@ impl ActionKindGet for TransactionPoolAction {
                 ActionKind::TransactionPoolApplyTransitionFrontierDiffWithAccounts
             }
             Self::Rebroadcast => ActionKind::TransactionPoolRebroadcast,
+        }
+    }
+}
+
+impl ActionKindGet for TransactionPoolEffectfulAction {
+    fn kind(&self) -> ActionKind {
+        match self {
+            Self::FetchAccounts { .. } => ActionKind::TransactionPoolEffectfulFetchAccounts,
         }
     }
 }
