1aedc548ec9cf8a66649aa8d08fec8eecc06bde2
Sventimir <svantimir@gmail.com>
2024-05-29 10:38:34 +0200

    Load more daemon settings from the configuration file.

--- node/src/daemon_json/mod.rs
+++ node/src/daemon_json/mod.rs
@@ -2,11 +2,13 @@ use serde::{Deserialize, Serialize};
 
 mod json_genesis;
 mod json_ledger;
+mod json_daemon;
 pub use json_genesis::Genesis;
 pub use json_ledger::{
     build_ledger_name, Account, AccountConfigError, AccountPermissions, AccountTiming, Ledger,
     Zkapp,
 };
+pub use json_daemon::Daemon;
 
 /// This type represents a JSON object loaded from daemon.json
 /// file. It does not describe its full structure, as it's not
@@ -20,6 +22,7 @@ pub use json_ledger::{
 /// that happens, the format can be extended to accommodate.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DaemonJson {
+    pub daemon: Option<Daemon>,
     pub ledger: Option<Ledger>,
     pub genesis: Option<Genesis>,
     pub epoch_data: Option<Epochs>,
@@ -101,5 +104,10 @@ mod test {
         } else {
             panic!("Expected Timed account");
         }
+        let daemon = daemon_json.daemon.unwrap();
+        assert_eq!(daemon.tx_pool_max_size(), 3000);
+        assert_eq!(daemon.peer_list_url(), None);
+        assert_eq!(daemon.slot_tx_end(), None);
+        assert_eq!(daemon.slot_chain_end(), None);
     }
 }
