afc6b2fccf8f06ca066afceb2fec8e4536c9d1c8
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2024-06-13 10:49:25 +0300

    fix(p2p): prune streams on disconnection
    fixes: #495

--- p2p/src/disconnection/p2p_disconnection_effects.rs
+++ p2p/src/disconnection/p2p_disconnection_effects.rs
@@ -1,9 +1,10 @@
+use openmina_core::error;
 use redux::ActionMeta;
 
 use super::{P2pDisconnectionAction, P2pDisconnectionService};
 
 impl P2pDisconnectionAction {
-    pub fn effects<Store, S>(&self, _: &ActionMeta, store: &mut Store)
+    pub fn effects<Store, S>(self, meta: &ActionMeta, store: &mut Store)
     where
         Store: crate::P2pStore<S>,
         Store::Service: P2pDisconnectionService,
@@ -16,8 +17,8 @@ impl P2pDisconnectionAction {
                 #[cfg(feature = "p2p-webrtc")]
                 {
                     // for webtrc, we use service
-                    store.service().disconnect(*peer_id);
-                    store.dispatch(P2pDisconnectionAction::Finish { peer_id: *peer_id });
+                    store.service().disconnect(peer_id);
+                    store.dispatch(P2pDisconnectionAction::Finish { peer_id });
                 }
                 #[cfg(feature = "p2p-libp2p")]
                 {
@@ -27,17 +28,21 @@ impl P2pDisconnectionAction {
                         .scheduler
                         .connections
                         .iter()
-                        .find(|(_, conn_state)| conn_state.peer_id() == Some(peer_id))
+                        .find(|(_, conn_state)| conn_state.peer_id() == Some(&peer_id))
                     {
                         store.dispatch(crate::P2pNetworkSchedulerAction::Disconnect {
                             addr: *addr,
                             reason: _reason.clone(),
                         });
-                        store.dispatch(P2pDisconnectionAction::Finish { peer_id: *peer_id });
+                    } else {
+                        error!(meta.time(); summary = "cannot find libp2p connection to disconnect", peer_id = display(peer_id));
                     }
+                    store.dispatch(P2pDisconnectionAction::Finish { peer_id });
                 }
             }
-            P2pDisconnectionAction::Finish { .. } => {}
+            P2pDisconnectionAction::Finish { peer_id } => {
+                store.dispatch(crate::P2pNetworkSchedulerAction::PruneStreams { peer_id });
+            }
         }
     }
 }
