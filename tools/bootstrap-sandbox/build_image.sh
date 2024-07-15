697019ae3b897f14394844cc636af5e93b86797b
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2024-07-15 13:33:49 +0200

    fix(bootstrap-replay): fixed some configs

--- tools/bootstrap-sandbox/build_image.sh
+++ tools/bootstrap-sandbox/build_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-docker build -t vladsimplestakingcom/bootstrap-rr:2.0.0berkeley-rc1 \
+docker build -t vladsimplestakingcom/bootstrap-rr:3.0.0-bullseye-devnet \
     -f tools/bootstrap-sandbox/Dockerfile target/record
 
-# docker run --rm --name openmina-bootstrap-replayer -p 8303:8303 vladsimplestakingcom/bootstrap-rr:2.0.0berkeley-rc1 openmina-bootstrap-sandbox --listen='/ip4/0.0.0.0/tcp/8303' replay 3830
+# docker run --rm --name openmina-bootstrap-replayer -p 8303:8303 vladsimplestakingcom/bootstrap-rr:3.0.0-bullseye-devnet openmina-bootstrap-sandbox --listen='/ip4/0.0.0.0/tcp/8303' replay 327636
