1b9fa42a0384f070368900d3b570f9d5777b4cac
Zura Benashvili <zura.benashvili@gmail.com>
2024-07-27 21:23:14 +0400

    fix(logging/wasm): not logging to console re: #84

--- node/common/src/tracing.rs
+++ node/common/src/tracing.rs
@@ -1,83 +1,102 @@
 pub use tracing::Level;
 
-use std::fmt::Result;
+#[cfg(not(target_family = "wasm"))]
+mod native {
+    use std::fmt::Result;
+    use tracing::{field::Visit, Level};
+    use tracing_subscriber::{
+        field::{RecordFields, VisitOutput},
+        fmt::{
+            format::{PrettyVisitor, Writer},
+            time::FormatTime,
+            FormatFields,
+        },
+    };
 
-use tracing::field::Visit;
-use tracing_subscriber::{
-    field::{RecordFields, VisitOutput},
-    fmt::{
-        format::{PrettyVisitor, Writer},
-        time::FormatTime,
-        FormatFields,
-    },
-};
-
-#[allow(unused)]
-fn redux_timer(w: &mut Writer<'_>) -> Result {
-    match redux::SystemTime::now().duration_since(redux::SystemTime::UNIX_EPOCH) {
-        Ok(v) => {
-            write!(w, "{}", v.as_nanos())
-        }
-        Err(_) => write!(w, "unknown-time"),
-    }
-}
-
-#[allow(dead_code)]
-struct ReduxTimer;
-
-impl FormatTime for ReduxTimer {
-    fn format_time(&self, w: &mut Writer<'_>) -> Result {
-        redux_timer(w)
-    }
-}
-
-struct FilterVisit<T>(T);
-
-impl<T> FilterVisit<T> {
-    fn into_inner(self) -> T {
-        self.0
-    }
-}
-
-impl<T> Visit for FilterVisit<T>
-where
-    T: Visit,
-{
-    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
-        if !field.name().starts_with("trace_") {
-            self.0.record_debug(field, value);
+    #[allow(unused)]
+    fn redux_timer(w: &mut Writer<'_>) -> Result {
+        match redux::SystemTime::now().duration_since(redux::SystemTime::UNIX_EPOCH) {
+            Ok(v) => {
+                write!(w, "{}", v.as_nanos())
+            }
+            Err(_) => write!(w, "unknown-time"),
         }
     }
-}
 
-#[derive(Default)]
-struct TracingFieldFormatter;
+    #[allow(dead_code)]
+    struct ReduxTimer;
 
-impl<'writer> FormatFields<'writer> for TracingFieldFormatter {
-    fn format_fields<R: RecordFields>(
-        &self,
-        writer: Writer<'writer>,
-        fields: R,
-    ) -> std::fmt::Result {
-        let mut v = FilterVisit(PrettyVisitor::new(writer, true));
-        fields.record(&mut v);
-        v.into_inner().finish()
+    impl FormatTime for ReduxTimer {
+        fn format_time(&self, w: &mut Writer<'_>) -> Result {
+            redux_timer(w)
+        }
     }
-}
 
-pub fn initialize(max_log_level: Level) {
-    let builder = tracing_subscriber::FmtSubscriber::builder()
+    struct FilterVisit<T>(T);
+
+    impl<T> FilterVisit<T> {
+        fn into_inner(self) -> T {
+            self.0
+        }
+    }
+
+    impl<T> Visit for FilterVisit<T>
+    where
+        T: Visit,
+    {
+        fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+            if !field.name().starts_with("trace_") {
+                self.0.record_debug(field, value);
+            }
+        }
+    }
+
+    #[derive(Default)]
+    struct TracingFieldFormatter;
+
+    impl<'writer> FormatFields<'writer> for TracingFieldFormatter {
+        fn format_fields<R: RecordFields>(
+            &self,
+            writer: Writer<'writer>,
+            fields: R,
+        ) -> std::fmt::Result {
+            let mut v = FilterVisit(PrettyVisitor::new(writer, true));
+            fields.record(&mut v);
+            v.into_inner().finish()
+        }
+    }
+
+    pub fn initialize(max_log_level: Level) {
+        let builder = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(max_log_level)
         .with_ansi(std::io::IsTerminal::is_terminal(&std::io::stdout()))
         .with_test_writer()
         //.with_timer(ReduxTimer)
         ;
-    if max_log_level != Level::TRACE {
-        let subscriber = builder.fmt_fields(TracingFieldFormatter).finish();
-        tracing::subscriber::set_global_default(subscriber)
-    } else {
-        let subscriber = builder.finish();
-        tracing::subscriber::set_global_default(subscriber)
+        if max_log_level != Level::TRACE {
+            let subscriber = builder.fmt_fields(TracingFieldFormatter).finish();
+            tracing::subscriber::set_global_default(subscriber)
+        } else {
+            let subscriber = builder.finish();
+            tracing::subscriber::set_global_default(subscriber)
+        }
+        .expect("global subscriber should be configurable");
     }
-    .expect("global subscriber should be configurable");
 }
+
+#[cfg(target_family = "wasm")]
+mod web {
+    use super::*;
+    use tracing_wasm::{set_as_global_default_with_config, WASMLayerConfigBuilder};
+
+    pub fn initialize(max_log_level: Level) {
+        let mut config = WASMLayerConfigBuilder::new();
+        config.set_max_level(max_log_level);
+        set_as_global_default_with_config(config.build());
+    }
+}
+
+#[cfg(not(target_family = "wasm"))]
+pub use native::initialize;
+#[cfg(target_family = "wasm")]
+pub use web::initialize;
