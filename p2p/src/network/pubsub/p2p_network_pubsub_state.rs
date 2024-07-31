5a0f0f908db2a26df9b9914971a4614c5f070cea
Vladislav Melnik <vladislav.melnik@protonmail.com>
2024-07-31 14:49:35 +0200

    feat(p2p/meshsub): graft

--- p2p/src/network/pubsub/p2p_network_pubsub_state.rs
+++ p2p/src/network/pubsub/p2p_network_pubsub_state.rs
@@ -3,24 +3,23 @@ use crate::{token::BroadcastAlgorithm, ConnectionAddr, PeerId, StreamId};
 use mina_p2p_messages::v2;
 use openmina_core::{snark::Snark, transaction::Transaction};
 use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::collections::{BTreeMap, VecDeque};
 
 #[derive(Default, Serialize, Deserialize, Debug, Clone)]
 pub struct P2pNetworkPubsubState {
     pub clients: BTreeMap<PeerId, P2pNetworkPubsubClientState>,
-    pub servers: BTreeMap<PeerId, ()>,
     pub seq: u64,
     pub to_sign: VecDeque<pb::Message>,
     pub seen: VecDeque<Vec<u8>>,
     pub incoming_block: Option<(PeerId, v2::MinaBlockBlockStableV2)>,
     pub incoming_transactions: Vec<(Transaction, u32)>,
     pub incoming_snarks: Vec<(Snark, u32)>,
+    pub topics: BTreeMap<String, BTreeMap<PeerId, P2pNetworkPubsubClientMeshState>>,
 }
 
 impl P2pNetworkPubsubState {
     pub fn prune_peer_state(&mut self, peer_id: &PeerId) {
         self.clients.remove(peer_id);
-        self.servers.remove(peer_id);
     }
 }
 
@@ -31,5 +30,24 @@ pub struct P2pNetworkPubsubClientState {
     pub outgoing_stream_id: Option<StreamId>,
     pub message: pb::Rpc,
     pub buffer: Vec<u8>,
-    pub topics: BTreeSet<String>,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct P2pNetworkPubsubClientMeshState {
+    pub mesh: P2pNetworkPubsubClientMeshAddingState,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum P2pNetworkPubsubClientMeshAddingState {
+    #[default]
+    Initial,
+    TheyRefused,
+    WeRefused,
+    Added,
+}
+
+impl P2pNetworkPubsubClientMeshState {
+    pub fn on_mesh(&self) -> bool {
+        matches!(&self.mesh, P2pNetworkPubsubClientMeshAddingState::Added)
+    }
 }
