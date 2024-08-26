b04ec0befc9c010e43baf11406c22541f22a4f25
Vladislav Melnik <vladislav.melnik@protonmail.com>
2024-08-26 15:35:10 +0200

    feat(p2p/meshsub): test in progress

--- node/testing/src/scenarios/multi_node/mod.rs
+++ node/testing/src/scenarios/multi_node/mod.rs
@@ -5,6 +5,8 @@ pub mod basic_connectivity_peer_discovery;
 
 #[cfg(feature = "p2p-libp2p")]
 pub mod connection_discovery;
+#[cfg(feature = "p2p-libp2p")]
+pub mod pubsub_advanced;
 pub mod vrf_correct_ledgers;
 pub mod vrf_correct_slots;
 pub mod vrf_epoch_bounds_correct_ledgers;
--- node/testing/tests/multi_node.rs
+++ node/testing/tests/multi_node.rs
@@ -16,3 +16,10 @@ scenario_test!(
     MultiNodeBasicConnectivityInitialJoining,
     MultiNodeBasicConnectivityInitialJoining
 );
+
+#[cfg(feature = "p2p-libp2p")]
+scenario_test!(
+    propagate_block,
+    openmina_node_testing::scenarios::multi_node::pubsub_advanced::MultiNodePubsubPropagateBlock,
+    openmina_node_testing::scenarios::multi_node::pubsub_advanced::MultiNodePubsubPropagateBlock
+);
