a5be7f1a161922726ba6cbb70f6f9bb91c4fcfc5
Sebastien Chapuis <sebastien@chapu.is>
2024-09-02 12:20:14 +0200

    Run `staged_ledger.rs` tests in wasm

--- ledger/src/staged_ledger/staged_ledger.rs
+++ ledger/src/staged_ledger/staged_ledger.rs
@@ -1994,6 +1994,9 @@ impl StagedLedger {
 
 #[cfg(test)]
 mod tests_ocaml {
+    #[cfg(target_family = "wasm")]
+    use wasm_bindgen_test::wasm_bindgen_test as test;
+
     use std::{
         collections::{BTreeSet, HashMap},
         panic::AssertUnwindSafe,
@@ -5845,6 +5848,9 @@ mod tests_ocaml {
 
 #[cfg(test)]
 mod tests {
+    #[cfg(target_family = "wasm")]
+    use wasm_bindgen_test::wasm_bindgen_test as test;
+
     use std::{collections::BTreeMap, fs::File};
 
     use mina_hasher::Fp;
