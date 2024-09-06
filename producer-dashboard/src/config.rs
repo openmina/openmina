0a77bbbcf12716ab449468e0c065b3ac0918440d
Adrian Nagy <adrian.nagy@minaprotocol.com>
2024-09-06 11:09:02 +0200

    feat: Add clap env for arguments

--- producer-dashboard/src/config.rs
+++ producer-dashboard/src/config.rs
@@ -9,19 +9,20 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 pub struct Config {
     /// RPC port for the proxy
-    #[arg(short, long, default_value_t = 3000)]
+    #[arg(short, long, env, default_value_t = 3000)]
     pub rpc_port: u16,
     // TODO(adonagy)
     /// Path to the producer's private key file
     ///
     /// MINA_PRIVKEY_PASS environmental variable must be set!
-    #[arg(short, long)]
-    pub private_key_path: PathBuf,
+    #[arg(short, long, env)]
+    pub producer_key: PathBuf,
 
-    /// Path to the database
-    #[arg(short, long, default_value = "/tmp/producer-dashboard")]
+    /// Path to the dashboard's database
+    #[arg(short, long, env, default_value = "/tmp/producer-dashboard")]
     pub database_path: PathBuf,
 
-    #[arg(short, long)]
+    /// Node's graphql endpoint URL
+    #[arg(short, long, env, default_value = "http://mina:5000/graphql")]
     pub node_url: String,
 }
--- producer-dashboard/src/main.rs
+++ producer-dashboard/src/main.rs
@@ -44,7 +44,7 @@ async fn main() {
     let password = std::env::var("MINA_PRIVKEY_PASS")
         .expect("Expected password in the variable `MINA_PRIVKEY_PASS`");
 
-    let key = AccountSecretKey::from_encrypted_file(config.private_key_path, &password)
+    let key = AccountSecretKey::from_encrypted_file(config.producer_key, &password)
         .expect("failed to decrypt secret key file");
     println!("[main] Producer key loaded");
 
