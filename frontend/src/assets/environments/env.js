c87196b8841a9876dd62f764fca781d1d72ccced
directcuteo <37619567+directcuteo@users.noreply.github.com>
2024-09-17 12:48:33 +0300

    Frontend - Prod Dynamic Environment (#714)

--- frontend/src/assets/environments/env.js
+++ frontend/src/assets/environments/env.js
@@ -5,4 +5,5 @@
  */
 export default {
   production: true,
+  configs: [],
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 import { MinaEnv } from '@shared/types/core/environment/mina-env.type';
-import env from '../assets/environments/env';
 
-export const environment: Readonly<MinaEnv> = env as MinaEnv;
+const env = (window as any).env;
+export const environment: Readonly<MinaEnv> = {
+  production: true,
+  configs: env.configs,
+  globalConfig: env.globalConfig,
+  hideNodeStats: env.hideNodeStats,
+  identifier: env.identifier,
+  hideToolbar: env.hideToolbar,
+};
