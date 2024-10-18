22f5c6d9cef234e3ad538bb87210119ef2c2230d
Adrian Nagy <adrian.nagy@minaprotocol.com>
2024-10-18 15:27:37 +0200

    feat: Add archive breadcrumb wire types

--- node/common/src/service/archive.rs
+++ node/common/src/service/archive.rs
@@ -1,24 +1,25 @@
+use mina_p2p_messages::v2::ArchiveBreadcrumb;
 use node::core::{channels::mpsc, thread};
 
 use super::NodeService;
 
 pub struct ArchiveService {
-    archive_sender: mpsc::UnboundedSender<()>,
+    archive_sender: mpsc::UnboundedSender<ArchiveBreadcrumb>,
 }
 
 impl ArchiveService {
-    fn new(archive_sender: mpsc::UnboundedSender<()>) -> Self {
+    fn new(archive_sender: mpsc::UnboundedSender<ArchiveBreadcrumb>) -> Self {
         Self { archive_sender }
     }
 
-    fn run(mut archive_receiver: mpsc::UnboundedReceiver<()>) {
-        while let Some(()) = archive_receiver.blocking_recv() {
+    fn run(mut archive_receiver: mpsc::UnboundedReceiver<ArchiveBreadcrumb>) {
+        while let Some(breadcrumb) = archive_receiver.blocking_recv() {
             println!("Sending data to archive");
         }
     }
 
     pub fn start() -> Self {
-        let (archive_sender, archive_receiver) = mpsc::unbounded_channel::<()>();
+        let (archive_sender, archive_receiver) = mpsc::unbounded_channel::<ArchiveBreadcrumb>();
 
         thread::Builder::new()
             .name("openmina_archive".to_owned())
@@ -32,7 +33,7 @@ impl ArchiveService {
 }
 
 impl node::transition_frontier::archive::archive_service::ArchiveService for NodeService {
-    fn send_to_archive(&mut self, data: ()) {
+    fn send_to_archive(&mut self, data: ArchiveBreadcrumb) {
         if let Some(archive) = self.archive.as_mut() {
             let _ = archive.archive_sender.send(data);
         }
--- node/src/transition_frontier/archive/archive_service.rs
+++ node/src/transition_frontier/archive/archive_service.rs
@@ -1,3 +1,5 @@
+use mina_p2p_messages::v2::ArchiveBreadcrumb;
+
 pub trait ArchiveService: redux::Service {
-    fn send_to_archive(&mut self, data: ());
+    fn send_to_archive(&mut self, data: ArchiveBreadcrumb);
 }
