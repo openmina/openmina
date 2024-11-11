d24aa09c52c65c21dbaf00d65e992a9e983297ce
directcuteo <37619567+directcuteo@users.noreply.github.com>
2024-11-11 18:13:36 +0200

    Frontend - Web node not supported screen (#885)

--- frontend/src/app/features/webnode/webnode.component.ts
+++ frontend/src/app/features/webnode/webnode.component.ts
@@ -1,16 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { WebNodeInitializationComponent } from '@app/features/webnode/web-node-initialization/web-node-initialization.component';
+import { Platform } from '@angular/cdk/platform';
+import { WebNodeNotSupportedComponent } from '@app/features/webnode/web-node-not-supported/web-node-not-supported.component';
 
 @Component({
   selector: 'mina-webnode',
   standalone: true,
   imports: [
     WebNodeInitializationComponent,
+    WebNodeNotSupportedComponent,
   ],
   templateUrl: './webnode.component.html',
   styleUrl: './webnode.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WebnodeComponent {
+export class WebnodeComponent implements OnInit {
 
+  supported: boolean = false;
+  isPhone: boolean = false;
+
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+    this.checkIfDeviceIsSupported();
+  }
+
+  private checkIfDeviceIsSupported(): void {
+    if (this.platform.IOS) {
+      this.supported = false;
+      this.isPhone = true;
+      return;
+    }
+
+    if (this.platform.FIREFOX || this.platform.BLINK) {
+      this.supported = false;
+      return;
+    }
+
+    this.supported = true;
+  }
 }
