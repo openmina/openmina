f3d8f506a6e98a53cc1a79f76a1318ed0643a2c5
Bruno Deferrari <bruno.deferrari@minaprotocol.com>
2024-11-27 21:21:10 -0300

    Split pubsub effect handler into many non-inlined functions for stack trace

--- p2p/src/network/pubsub/pubsub_effectful/p2p_network_pubsub_effectful_effects.rs
+++ p2p/src/network/pubsub/pubsub_effectful/p2p_network_pubsub_effectful_effects.rs
@@ -6,6 +6,7 @@ use super::super::pb;
 
 use crate::{
     P2pCryptoService, P2pNetworkConnectionError, P2pNetworkPubsubAction, P2pNetworkSchedulerAction,
+    PeerId,
 };
 
 use super::P2pNetworkPubsubEffectfulAction;
@@ -16,78 +17,91 @@ impl P2pNetworkPubsubEffectfulAction {
         Store: crate::P2pStore<S>,
         Store::Service: P2pCryptoService,
     {
-        let state = &store.state().network.scheduler.broadcast_state;
-
         match self {
             P2pNetworkPubsubEffectfulAction::Sign { author, topic } => {
-                if let Some(to_sign) = state.to_sign.front() {
-                    let mut publication = vec![];
-                    if prost::Message::encode(to_sign, &mut publication).is_err() {
-                        store.dispatch(P2pNetworkPubsubAction::SignError { author, topic });
-                    } else {
-                        let signature = store.service().sign_publication(&publication).into();
-                        store.dispatch(P2pNetworkPubsubAction::BroadcastSigned { signature });
-                    }
-                }
+                handle_sign(author, topic, store);
             }
             P2pNetworkPubsubEffectfulAction::IncomingData {
                 peer_id,
                 seen_limit,
             } => {
-                let Some(state) = state.clients.get(&peer_id) else {
-                    // TODO: investigate, cannot reproduce this
-                    // bug_condition!("{:?} not found in state.clients", peer_id);
+                handle_incoming_data(peer_id, seen_limit, store);
+            }
+        }
+    }
+}
+
+#[inline(never)]
+fn handle_sign<Store, S>(author: PeerId, topic: String, store: &mut Store)
+where
+    Store: crate::P2pStore<S>,
+    Store::Service: P2pCryptoService,
+{
+    let state = &store.state().network.scheduler.broadcast_state;
+
+    if let Some(to_sign) = state.to_sign.front() {
+        let mut publication = vec![];
+        if prost::Message::encode(to_sign, &mut publication).is_err() {
+            store.dispatch(P2pNetworkPubsubAction::SignError { author, topic });
+        } else {
+            let signature = store.service().sign_publication(&publication).into();
+            store.dispatch(P2pNetworkPubsubAction::BroadcastSigned { signature });
+        }
+    }
+}
+
+#[inline(never)]
+fn handle_incoming_data<Store, S>(peer_id: PeerId, seen_limit: usize, store: &mut Store)
+where
+    Store: crate::P2pStore<S>,
+    Store::Service: P2pCryptoService,
+{
+    let state = &store.state().network.scheduler.broadcast_state;
+
+    let Some(state) = state.clients.get(&peer_id) else {
+        return;
+    };
+    let messages = state.incoming_messages.clone();
+
+    for message in messages {
+        let result = if let Some(signature) = message.signature.clone() {
+            if let Ok(Some(pk)) = originator(&message) {
+                let mut message = message;
+                let Ok(data) = encode_without_signature_and_key(&mut message) else {
+                    bug_condition!("serialization error");
                     return;
                 };
-                let messages = state.incoming_messages.clone();
+                let message = message;
 
-                for message in messages {
-                    let result = if let Some(signature) = message.signature.clone() {
-                        if let Ok(Some(pk)) = originator(&message) {
-                            // the message is mutable only in this function
-                            let mut message = message;
-                            let Ok(data) = encode_without_signature_and_key(&mut message) else {
-                                // should never happen;
-                                // we just decode this message, so it should encode without error
-                                bug_condition!("serialization error");
-                                return;
-                            };
-                            // keep the binding immutable
-                            let message = message;
-
-                            if store.service().verify_publication(&pk, &data, &signature) {
-                                store.dispatch(P2pNetworkPubsubAction::IncomingMessage {
-                                    peer_id,
-                                    message,
-                                    seen_limit,
-                                });
-                                Ok(())
-                            } else {
-                                Err("invalid signature")
-                            }
-                        } else {
-                            Err("message doesn't contain verifying key")
-                        }
-                    } else {
-                        Err("message doesn't contain signature")
-                    };
-
-                    if let Err(error) = result {
-                        let Some((addr, _)) = store.state().network.scheduler.find_peer(&peer_id)
-                        else {
-                            bug_condition!("{:?} not found in scheduler state", peer_id);
-                            return;
-                        };
-
-                        store.dispatch(P2pNetworkSchedulerAction::Error {
-                            addr: *addr,
-                            error: P2pNetworkConnectionError::PubSubError(error.to_string()),
-                        });
-
-                        return;
-                    }
+                if store.service().verify_publication(&pk, &data, &signature) {
+                    store.dispatch(P2pNetworkPubsubAction::IncomingMessage {
+                        peer_id,
+                        message,
+                        seen_limit,
+                    });
+                    Ok(())
+                } else {
+                    Err("invalid signature")
                 }
+            } else {
+                Err("message doesn't contain verifying key")
             }
+        } else {
+            Err("message doesn't contain signature")
+        };
+
+        if let Err(error) = result {
+            let Some((addr, _)) = store.state().network.scheduler.find_peer(&peer_id) else {
+                bug_condition!("{:?} not found in scheduler state", peer_id);
+                return;
+            };
+
+            store.dispatch(P2pNetworkSchedulerAction::Error {
+                addr: *addr,
+                error: P2pNetworkConnectionError::PubSubError(error.to_string()),
+            });
+
+            return;
         }
     }
 }
