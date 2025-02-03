66a4ce48be1ebcf7943200ff99f853aae5bae6cc
Daniel Kuehr <daniel.kuehr@viablesystems.io>
2025-02-03 10:50:37 -0500

    fix: replace unwrap for non-panicking code

--- ledger/src/proofs/step.rs
+++ ledger/src/proofs/step.rs
@@ -1948,8 +1948,10 @@ pub fn expand_deferred(params: ExpandDeferredParams) -> Result<DeferredValues<Fp
     let zeta = ScalarChallenge::limbs_to_field(&plonk0.zeta_bytes);
     let alpha = ScalarChallenge::limbs_to_field(&plonk0.alpha_bytes);
     let step_domain: u8 = proof_state.deferred_values.branch_data.domain_log2.as_u8();
-    let domain: Radix2EvaluationDomain<Fp> =
-        Radix2EvaluationDomain::new(1 << step_domain as u64).unwrap();
+    let Some(domain) = Radix2EvaluationDomain::<Fp>::new(1 << step_domain as u64) else {
+        return Err(InvalidBigInt);
+    };
+
     let zetaw = zeta * domain.group_gen;
 
     let plonk_minimal = PlonkMinimal::<Fp, 4> {
