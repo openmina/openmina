0424d9f46099ed7854ab2eb560e5ce382a4ea249
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2025-02-10 11:44:17 +0100

    (feat/webrtc-sniffer): add filter parameter

--- tools/webrtc-sniffer/src/lib.rs
+++ tools/webrtc-sniffer/src/lib.rs
@@ -13,11 +13,7 @@ pub fn run<T: Activated + ?Sized>(
 
     for item in net::UdpIter::new(capture, file) {
         let (src, dst, data) = item?;
-        log::info!(
-            "{src} -> {dst}: {} {}",
-            data.len(),
-            hex::encode(&data[..data.len().min(12)])
-        );
+        log::info!("{src} -> {dst}: {} {}", data.len(), hex::encode(data));
     }
 
     Ok(())
