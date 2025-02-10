db7206cd456a4540cc55704f5b04f901f60e7a14
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2025-02-10 13:30:34 +0100

    (feat/webrtc-sniffer): implement dtls parser

--- tools/webrtc-sniffer/src/lib.rs
+++ tools/webrtc-sniffer/src/lib.rs
@@ -1,56 +1,71 @@
 mod net;
 
+mod dtls;
+
+use std::{borrow::Cow, collections::BTreeMap, fmt, net::SocketAddr};
+
 use p2p::identity::SecretKey;
 
 use pcap::{Activated, Capture, Savefile};
+
+type State = dtls::State;
+
+#[derive(Clone, Copy)]
+pub struct MsgHeader {
+    src: SocketAddr,
+    dst: SocketAddr,
+    len: u16,
+}
+
+impl fmt::Display for MsgHeader {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let MsgHeader { src, dst, len } = self;
+        write!(f, "{src} -> {dst} {len}")
+    }
+}
 
 pub fn run<T: Activated + ?Sized>(
     capture: Capture<T>,
     file: Option<Savefile>,
     secret_key: SecretKey,
 ) -> Result<(), net::DissectError> {
-    let _ = secret_key;
+    let mut connections = BTreeMap::<(SocketAddr, SocketAddr), State>::new();
 
+    let mut buffer = None::<Vec<u8>>;
     for item in net::UdpIter::new(capture, file) {
         let (src, dst, data) = item?;
-        log::info!("{src} -> {dst}: {} {}", data.len(), hex::encode(data));
+
+        let hdr = MsgHeader {
+            src,
+            dst,
+            len: data.len() as _,
+        };
+
+        // skip STUN/TURN
+        if data[4..8].eq(b"\x21\x12\xa4\x42") {
+            continue;
+        }
+
+        let data = if let Some(mut buffer) = buffer.take() {
+            buffer.extend_from_slice(&data);
+            Cow::Owned(buffer)
+        } else {
+            Cow::Borrowed(data.as_ref())
+        };
+
+        let res = if let Some(cn) = connections.get_mut(&(src, dst)) {
+            cn.handle(hdr, &data, true)
+        } else {
+            connections
+                .entry((dst, src))
+                .or_insert_with(|| State::new(secret_key.clone()))
+                .handle(hdr, &data, false)
+        };
+
+        if let Err(nom::Err::Incomplete(_)) = res {
+            buffer = Some(data.into_owned());
+        }
     }
 
     Ok(())
-}
-
-pub fn handle(packet: pcap::Packet) {
-    use std::net::{IpAddr, SocketAddr};
-
-    use etherparse::{NetSlice, SlicedPacket, TransportSlice};
-
-    let eth = SlicedPacket::from_ethernet(packet.data).unwrap();
-    if let (Some(net), Some(transport)) = (eth.net, eth.transport) {
-        let (src_ip, dst_ip) = match net {
-            NetSlice::Ipv4(ip) => (
-                IpAddr::V4(ip.header().source().into()),
-                IpAddr::V4(ip.header().destination().into()),
-            ),
-            NetSlice::Ipv6(ip) => (
-                IpAddr::V6(ip.header().source().into()),
-                IpAddr::V6(ip.header().destination().into()),
-            ),
-            NetSlice::Arp(_) => return,
-        };
-        let (src_port, dst_port, slice) = match transport {
-            TransportSlice::Udp(udp) => (udp.source_port(), udp.destination_port(), udp.payload()),
-            _ => return,
-        };
-
-        let (src, dst, data) = (
-            SocketAddr::new(src_ip, src_port),
-            SocketAddr::new(dst_ip, dst_port),
-            slice,
-        );
-        log::info!(
-            "{src} -> {dst}: {} {}",
-            data.len(),
-            hex::encode(&data[..data.len().min(12)])
-        );
-    }
 }
