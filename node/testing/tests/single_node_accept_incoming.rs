05f78731a35404c563eb578dc6bf659a91b650f4
Grga <mimirz@proton.me>
2025-12-02 09:57:34 +0100

    Update node/testing/tests/single_node_accept_incoming.rs
    Co-authored-by: Richard Pringle <rpring9@gmail.com>

--- node/testing/tests/single_node_accept_incoming.rs
+++ node/testing/tests/single_node_accept_incoming.rs
@@ -1,9 +1,9 @@
-#[cfg(not(feature = "p2p-webrtc"))]
+#![cfg(not(feature = "p2p-webrtc"))]
+
 use mina_node_testing::scenarios::solo_node::basic_connectivity_accept_incoming::SoloNodeBasicConnectivityAcceptIncoming;
 
 mod common;
 
-#[cfg(not(feature = "p2p-webrtc"))]
 scenario_test!(
     accept_incoming,
     SoloNodeBasicConnectivityAcceptIncoming,
